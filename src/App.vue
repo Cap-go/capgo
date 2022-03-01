a8007ccf070d1d65d4d29055f044814dfd18d5de
martindonadieu <martindonadieu@gmail.com>
2022-03-01 16:53:08 +0000

    fix: favicons

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,19 @@ import { useHead } from '@vueuse/head'
 // https://github.com/vueuse/head
 // you can use this to manipulate the document head in any components,
 useHead({
-  title: 'Capgo',
+  title: 'Capgo | Capacitor tooling',
   meta: [
     { name: 'description', content: 'App to test your capacitor app without store hassle' },
+    { name: 'og:type', content: 'website' },
+    { name: 'og:title', content: 'Capgo | Capacitor tooling' },
+    { name: 'twitter:title', content: 'Capgo | Capacitor tooling' },
+    { name: 'twitter:card', content: 'summary_large_image' },
+    { name: 'twitter:domain', content: 'captime.app' },
+    { name: 'twitter:image', content: 'https://capgo.app/alternative_to_1.png' },
+    { name: 'og:image', content: 'https://capgo.app/alternative_to_1.png' },
+    { name: 'og:alt', content: 'crossfit image' },
   ],
 })
-
 </script>
 
 <template>
