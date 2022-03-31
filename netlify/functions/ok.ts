ff99493e53cbf2d5f2ed6a9ef3899a492f764c1c
martindonadieu <martindonadieu@gmail.com>
2022-03-31 15:16:23 +0100

    fix: remove netlify function from capgo app

--- netlify/functions/ok.ts
+++
@@ -1,7 +0,0 @@
-import type { Handler } from '@netlify/functions'
-import { sendRes } from './../services/utils'
-
-export const handler: Handler = async(event) => {
-  console.log(event.httpMethod)
-  return sendRes()
-}
