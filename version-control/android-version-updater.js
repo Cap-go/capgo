12f6888d8c290644562cd6dd3f4291abb15a7779
martindonadieu <martindonadieu@gmail.com>
2022-04-27 13:30:40 +0100

    fix: issue android bump

--- version-control/android-version-updater.js
+++ version-control/android-version-updater.js
@@ -1,11 +1,11 @@
-module.exports.readVersion = function (contents) {
-  let version = contents.split('versionName "')[1].split('"')[0];
-  return version;
+module.exports.readVersion = function(contents) {
+  const version = contents.split('versionName "')[1].split('"')[0]
+  return version
 }
 
-module.exports.writeVersion = function (contents, version) {
-  let newContent = contents.replace(/(.*(?:versionName[ \t]+).*)/g, `        versionName "${version}"`);
-  let versionCode = Number(version.split('.').map(v => v.length === 1 ? `0${v}` : v).join('')) 
-  let finalContent = newContent.replace(/(.*(?:versionName[ \t]+).*)/g, `        versionName "${versionCode}"`);
+module.exports.writeVersion = function(contents, version) {
+  const newContent = contents.replace(/(.*(?:versionName[ \t]+).*)/g, `        versionName "${version}"`)
+  const versionCode = Number(version.split('.').map(v => v.length === 1 ? `0${v}` : v).join(''))
+  const finalContent = newContent.replace(/(.*(?:versionCode[ \t]+).*)/g, `        versionCode "${versionCode}"`)
   return finalContent
 }
