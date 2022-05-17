d442667d03813c1f3ef59974b9d715b1ceeadf4c
martindonadieu <martindonadieu@gmail.com>
2022-05-17 19:30:54 +0100

    fix: app_stats function

--- supabase/functions/app_stats/index.ts
+++ supabase/functions/app_stats/index.ts
@@ -5,21 +5,29 @@ import { sendRes } from '../_utils/utils.ts'
 
 const getApp = (appId: string) => {
   const supabase = supabaseAdmin
+  const now = new Date()
+  const firstDay = new Date(now.getFullYear(), now.getMonth(), 1)
+  const lastDay = new Date(now.getFullYear(), now.getMonth() + 1, 0)
+  // console.log('req', req)
   return {
     downloads: supabase
       .from<definitions['stats']>('stats')
-      .select()
+      .select('*', { count: 'exact', head: true })
       .eq('app_id', appId)
-      .eq('action', 'get'),
+      .lte('created_at', lastDay.toISOString())
+      .gte('created_at', firstDay.toISOString())
+      .eq('action', 'set'),
     versions: supabase
       .from<definitions['app_versions']>('app_versions')
-      .select('*', { count: 'exact' })
+      .select('id', { count: 'exact', head: true })
       .eq('app_id', appId),
     shared: supabase
-      .from<definitions['channel_users']>('channel_users'),
+      .from<definitions['channel_users']>('channel_users')
+      .select('id', { count: 'exact', head: true })
+      .eq('app_id', appId),
     channels: supabase
       .from<definitions['channels']>('channels')
-      .select('*', { count: 'exact' })
+      .select('id', { count: 'exact', head: true })
       .eq('app_id', appId),
   }
 }
@@ -32,8 +40,8 @@ serve(async(event: Request) => {
     return sendRes({ status: 'Cannot find authorization secret' }, 400)
   }
   if (!authorizationSecret || !API_SECRET || authorizationSecret !== API_SECRET) {
-    console.error('Fail Authorization')
-    return sendRes({ message: 'Fail Authorization' }, 400)
+    console.error('Fail Authorization', authorizationSecret, API_SECRET)
+    return sendRes({ message: 'Fail Authorization', authorizationSecret, API_SECRET }, 400)
   }
   try {
     const { data: apps } = await supabase
@@ -47,20 +55,21 @@ serve(async(event: Request) => {
     for (const app of apps) {
       if (!app.id)
         continue
-      const res = getApp(app.id)
+      const res = getApp(app.app_id)
       all.push(Promise.all([app, res.downloads, res.versions, res.shared, res.channels])
         .then(([app, downloads, versions, shared, channels]) => {
-          if (!app.id)
+          if (!app.app_id)
             return
+          // console.log('app', app.app_id, downloads, versions, shared, channels)
           const newData: definitions['app_stats'] = {
             app_id: app.app_id,
             user_id: app.user_id,
-            channels: channels.data?.length || 0,
-            mlu: downloads.data?.length || 0,
-            versions: versions.data?.length || 0,
-            shared: shared.data?.length || 0,
+            channels: channels.count || 0,
+            mlu: downloads.count || 0,
+            versions: versions.count || 0,
+            shared: shared.count || 0,
           }
-          console.log('newData', newData)
+          // console.log('newData', newData)
           return supabase
             .from<definitions['app_stats']>('app_stats')
             .upsert(newData)
