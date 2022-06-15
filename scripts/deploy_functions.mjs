466775439748f21ef425b14419ae3e0177e4d042
martindonadieu <martindonadieu@gmail.com>
2022-06-16 00:01:00 +0100

    fix: build functions in  CI

--- scripts/deploy_functions.mjs
+++ scripts/deploy_functions.mjs
@@ -2,32 +2,42 @@ import fs from 'fs'
 import util from 'util'
 import { exec as execCb } from 'child_process'
 import { exit } from 'process'
+import { homedir } from 'os'
+import { supa_url } from './utils.mjs'
 
 const exec = util.promisify(execCb)
 const folders = fs.readdirSync('./supabase/functions').filter(file => !file.startsWith('_'))
 const calls = []
-const functions = []
+
+const token = process.env.SUPABASE_TOKEN || ''
+const projectRef = supa_url.split('.')[0].replace('https://', '')
 
 folders.forEach((folder) => {
   const file = `./supabase/functions/${folder}/.no_verify_jwt`
   let command = `supabase functions deploy ${folder}`
-  if (fs.existsSync(file))
+  let no_verify_jwt = false
+  if (fs.existsSync(file)) {
     command += ' --no-verify-jwt'
-  calls.push(exec(command))
-  functions.push({
-    name: folder,
-    no_verify_jwt: fs.existsSync(file),
-  })
+    no_verify_jwt = true
+  }
+  calls.push(exec(command).then((r) => {
+    if (r.stderr)
+      console.error(folder, r.stderr)
+    else
+      console.log(`Upload done for ${folder}${no_verify_jwt ? ' no_verify_jwt' : ''} ✅`)
+    return r
+  }))
 })
 
 try {
-  const res = await Promise.all(calls)
-  res.forEach((r, i) => {
-    if (r.stderr)
-      console.error(r.stderr)
-    else;
-    console.log(`Upload done for ${functions[i].name}${functions[i].no_verify_jwt ? ' no_verify_jwt' : ''} ✅`)
-  })
+  if (!token) {
+    console.error('SUPABASE_TOKEN is not set')
+    exit(1)
+  }
+  fs.writeFileSync(`${homedir()}/.supabase/access-token`, token)
+  fs.writeFileSync('supabase/.temp/project-ref', projectRef)
+  console.log('projectRef', projectRef)
+  await Promise.all(calls)
 }
 catch (e) {
   console.error(e) // should contain code (exit code) and signal (that caused the termination).
