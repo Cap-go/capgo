188972f77c943516bbf3fdd11ac961131a3a85e2
martindonadieu <martindonadieu@gmail.com>
2022-07-06 08:51:31 +0200

    fix: script use var ENV

--- scripts/deploy_functions.mjs
+++ scripts/deploy_functions.mjs
@@ -2,7 +2,6 @@ import { existsSync, readdirSync } from 'fs'
 import { promisify } from 'util'
 import { exec as execCb } from 'child_process'
 import { exit } from 'process'
-import { homedir } from 'os'
 import { outputFile } from 'fs-extra'
 import { supa_url } from './utils.mjs'
 
@@ -10,17 +9,11 @@ const exec = promisify(execCb)
 const folders = readdirSync('./supabase/functions')
   .filter(file => !file.startsWith('_'))
 
-const token = process.env.SUPABASE_TOKEN || ''
 const projectRef = supa_url.split('.')[0].replace('https://', '')
 
 try {
   console.log('projectRef', projectRef)
   await outputFile('./supabase/.temp/project-ref', projectRef)
-  if (!token) {
-    console.error('SUPABASE_TOKEN is not set')
-    exit(1)
-  }
-  await outputFile(`${homedir()}/.supabase/access-token`, token)
   // for in folders
   for (const folder of folders) {
     const fileNoJWT = `./supabase/functions/${folder}/.no_verify_jwt`
