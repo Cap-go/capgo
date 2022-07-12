0e8f35a68c8be588c3bcce313cc61800ea13d724
martindonadieu <martindonadieu@gmail.com>
2022-07-12 12:14:48 +0100

    fi: issue in app for http native

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -13,7 +13,7 @@ export const useSupabase = () => {
     detectSessionInUrl: false,
     fetch: (requestInfo, requestInit) => {
       const url = requestInfo.toString()
-      if (requestInit?.method === 'POST' && url.includes('/storage/'))
+      if (requestInit?.method === 'POST' && (url.includes('/storage/') || url.includes('.functions.supabase.co')))
         return fetch(requestInfo, requestInit)
       return Http.request({
         url,
