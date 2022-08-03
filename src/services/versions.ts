58379cbbc18c4a03d981f8f0bb9fbe9495526345
martindonadieu <martindonadieu@gmail.com>
2022-08-03 02:12:16 +0100

    fix: issue updater

--- src/services/versions.ts
+++ src/services/versions.ts
@@ -1,5 +1,6 @@
 import { isPlatform, loadingController, toastController } from '@ionic/vue'
 import { CapacitorUpdater } from '@capgo/capacitor-updater'
+import dayjs from 'dayjs'
 import { useSupabase } from './supabase'
 import type { definitions } from '~/types/supabase'
 
@@ -28,25 +29,22 @@ export const openVersion = async (app: definitions['app_versions'], userId: stri
 
   if (signedURL && isPlatform('capacitor')) {
     try {
+      // const newBundle = await CapacitorUpdater.download({
+      //   url: signedURL
+      // })
+      // await CapacitorUpdater.set(newBundle)
+      // comment temporary
       const newBundle = await CapacitorUpdater.download({
         url: signedURL,
         version: app.name,
       })
-      await CapacitorUpdater.set({
-        version: newBundle.version,
-        versionName: app.name,
-      })
-      // comment temporary
-      // const newBundle = await CapacitorUpdater.download({
-      //   url: signedURL,
-      // })
-      // const current = await CapacitorUpdater.current()
+      const current = await CapacitorUpdater.current()
       // console.log('current', current)
-      // await CapacitorUpdater.next({ id: current.bundle.id })
-      // // iso date in one hour
-      // const expires = new Date(Date.now() + 1000 * 60 * 60).toISOString()
-      // await CapacitorUpdater.setDelay({ kind: 'date', value: expires })
-      // await CapacitorUpdater.set(newBundle)
+      await CapacitorUpdater.next({ id: current.bundle.id })
+      // // iso date in one hour with dayjs
+      const expires = dayjs().add(1, 'hour').toISOString()
+      await CapacitorUpdater.setDelay({ kind: 'date', value: expires })
+      await CapacitorUpdater.set(newBundle)
     }
     catch (error) {
       console.error('Error', error)
