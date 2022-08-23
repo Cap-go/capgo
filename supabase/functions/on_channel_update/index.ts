ea3fc763d9f2c0b5fedb02e7577a73644a4466e2
martindonadieu <martindonadieu@gmail.com>
2022-08-23 02:36:15 +0100

    feat: add auto unpublic other channels

---
+++ supabase/functions/on_channel_update/index.ts
@@ -0,0 +1,40 @@
+import { serve } from 'https://deno.land/std@0.152.0/http/server.ts'
+import { supabaseAdmin } from '../_utils/supabase.ts'
+import type { definitions } from '../_utils/types_supabase.ts'
+import { sendRes } from '../_utils/utils.ts'
+
+// Generate a v4 UUID. For this we use the browser standard `crypto.randomUUID`
+// function.
+serve(async (event: Request) => {
+  const API_SECRET = Deno.env.get('API_SECRET')
+  const authorizationSecret = event.headers.get('apisecret')
+  if (!authorizationSecret)
+    return sendRes({ status: 'Cannot find authorization secret' }, 400)
+  if (!authorizationSecret || !API_SECRET || authorizationSecret !== API_SECRET) {
+    console.log('Fail Authorization')
+    return sendRes({ message: 'Fail Authorization' }, 400)
+  }
+  try {
+    console.log('body')
+    const body = (await event.json()) as { record: definitions['channels'] }
+    const record = body.record
+
+    if (record.public) {
+      // find all other channels with same app_i with public true and update them to false
+      await supabaseAdmin
+        .from< definitions['channels']>('channels')
+        .update({ public: false })
+        .eq('app_id', record.app_id)
+        .eq('public', true)
+        .neq('id', record.id)
+    }
+    return sendRes()
+  }
+  catch (e) {
+    console.log('Error', e)
+    return sendRes({
+      status: 'Error unknow',
+      error: JSON.stringify(e),
+    }, 500)
+  }
+})
