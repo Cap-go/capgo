cbf4cbbf50200669a3729577abb92e128ba2287d
martindonadieu <martindonadieu@gmail.com>
2022-08-31 00:34:03 +0100

    fix: issue plausible tracking

--- src/main.ts
+++ src/main.ts
@@ -42,7 +42,7 @@ const app = createApp(App)
 const routes = [...setupLayouts(generatedRoutes), { path: '/app', redirect: '/app/home' }, { path: '/', redirect: '/login' }]
 const router = createRouter({ history: createWebHistory(import.meta.env.BASE_URL), routes })
 app.use(router)
-initPlausible(import.meta.env.domain as string)
+initPlausible(import.meta.env.pls_domain as string)
 // install all modules under `modules/`
 type UserModule = (ctx: { app: typeof app; router: Router }) => void
 
