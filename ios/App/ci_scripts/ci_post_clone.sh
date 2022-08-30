7d252d7c79423e0092ab1aff59cdd48d55a00e94
martindonadieu <martindonadieu@gmail.com>
2022-08-30 22:37:58 +0100

    fix: path of post_clone

--- ios/App/ci_scripts/ci_post_clone.sh
+++ ios/App/ci_scripts/ci_post_clone.sh
@@ -3,10 +3,13 @@
 echo "POST CLONE"
 set -x
 
-# Install CocoaPods and yarn using Homebrew.
+export HOMEBREW_NO_INSTALL_CLEANUP=TRUE
 brew install cocoapods
+# have to add node yourself
 brew install node@16
+# link it to the path
 brew link node@16
+
 brew install pnpm
 
 # Install dependencies
