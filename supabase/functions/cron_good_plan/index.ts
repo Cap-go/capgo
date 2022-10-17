f3b5d9602707a36e131b8bd18ed3c4a6811a5f3a
GitHub <noreply@github.com>
2022-10-17 19:52:30 +0000

    Update dependencies

--- supabase/functions/cron_good_plan/index.ts
+++ supabase/functions/cron_good_plan/index.ts
@@ -1,4 +1,4 @@
-import { serve } from 'https://deno.land/std@0.159.0/http/server.ts'
+import { serve } from 'https://deno.land/std@0.160.0/http/server.ts'
 import { supabaseAdmin } from '../_utils/supabase.ts'
 import type { definitions } from '../_utils/types_supabase.ts'
 import { sendRes } from '../_utils/utils.ts'
--- supabase/functions/version/index.ts
+++ supabase/functions/version/index.ts
@@ -1,4 +1,4 @@
-import { serve } from 'https://deno.land/std@0.159.0/http/server.ts'
+import { serve } from 'https://deno.land/std@0.160.0/http/server.ts'
 import { checkAppOwner, supabaseAdmin } from '../_utils/supabase.ts'
 import type { definitions } from '../_utils/types_supabase.ts'
 import { checkKey, sendRes } from '../_utils/utils.ts'
