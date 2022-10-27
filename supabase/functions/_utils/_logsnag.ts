548205e851d2493dc34402c6a3538d4397060a8d
martindonadieu <martindonadieu@gmail.com>
2022-10-27 13:18:47 +0800

    fix: disable logsnag in dev

--- supabase/functions/_utils/_logsnag.ts
+++ supabase/functions/_utils/_logsnag.ts
@@ -1,9 +1,11 @@
 import { LogSnag } from 'https://cdn.logsnag.com/deno/0.1.5/index.ts'
 
-const lsg = new LogSnag({
-  token: Deno.env.get('LOGSNAG_TOKEN') || '',
-  project: Deno.env.get('LOGSNAG_PROJECT') || '',
-})
+const lsg = Deno.env.get('LOGSNAG_TOKEN')
+  ? new LogSnag({
+    token: Deno.env.get('LOGSNAG_TOKEN') || '',
+    project: Deno.env.get('LOGSNAG_PROJECT') || '',
+  })
+  : { publish: () => {}, insight: () => {} }
 
 const insight = async (data: { title: string; value: number; icon: string }[]) => {
   const all = []
