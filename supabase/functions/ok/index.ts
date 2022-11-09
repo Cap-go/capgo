bdc8d8e70d814b8493312decb5950e529da05b1c
GitHub <noreply@github.com>
2022-11-09 19:43:37 +0000

    Update dependencies

--- supabase/functions/ok/index.ts
+++ supabase/functions/ok/index.ts
@@ -1,4 +1,4 @@
-import { serve } from 'https://deno.land/std@0.162.0/http/server.ts'
+import { serve } from 'https://deno.land/std@0.163.0/http/server.ts'
 import { sendRes } from '../_utils/utils.ts'
 import { supabaseAdmin } from '../_utils/supabase.ts'
 import type { definitions } from '../_utils/types_supabase.ts'
