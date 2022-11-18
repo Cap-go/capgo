1cae33879f6d976191716179663ad4d4ff90e9ad
GitHub <noreply@github.com>
2022-11-18 19:43:49 +0000

    Update dependencies

--- supabase/functions/app_stats/index.ts
+++ supabase/functions/app_stats/index.ts
@@ -1,4 +1,4 @@
-import { serve } from 'https://deno.land/std@0.164.0/http/server.ts'
+import { serve } from 'https://deno.land/std@0.165.0/http/server.ts'
 import { supabaseAdmin, updateOrAppStats } from '../_utils/supabase.ts'
 import type { AppStatsIncrement } from '../_utils/supabase.ts'
 import type { definitions } from '../_utils/types_supabase.ts'
