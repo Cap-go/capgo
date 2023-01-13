273791b06f1707217c72b537d4162b2d5329b460
Martin Donadieu <martindonadieu@gmail.com>
2023-01-13 15:43:42 +0800

    fix: remove duplicate code

--- supabase/functions/_utils/r2.ts
+++ supabase/functions/_utils/r2.ts
@@ -4,52 +4,36 @@ const accountid = Deno.env.get('R2_ACCOUNT_ID')
 const access_key_id = Deno.env.get('R2_ACCESS_KEY_ID')
 const access_key_secret = Deno.env.get('R2_SECRET_ACCESS_KEY')
 
+const initR2 = () => new S3Client({
+  endPoint: `https://${accountid}.r2.cloudflarestorage.com`,
+  region: 'us-east-1',
+  bucket: 'capgo',
+  accessKey: access_key_id,
+  secretKey: access_key_secret,
+})
+
 export const upload = (file: Blob) => {
   const id = crypto.randomUUID()
-  const s3client = new S3Client({
-    endPoint: `https://${accountid}.r2.cloudflarestorage.com`,
-    region: 'us-east-1',
-    bucket: 'capgo',
-    accessKey: access_key_id,
-    secretKey: access_key_secret,
-  })
+  const s3client = initR2()
 
   // Upload a file:
   return s3client.putObject(id, file.stream())
 }
 
 export const deleteObject = (fileId: string) => {
-  const s3client = new S3Client({
-    endPoint: `https://${accountid}.r2.cloudflarestorage.com`,
-    region: 'us-east-1',
-    bucket: 'capgo',
-    accessKey: access_key_id,
-    secretKey: access_key_secret,
-  })
+  const s3client = initR2()
   return s3client.deleteObject(fileId)
 }
 
 export const checkIfExist = (fileId: string) => {
-  const s3client = new S3Client({
-    endPoint: `https://${accountid}.r2.cloudflarestorage.com`,
-    region: 'us-east-1',
-    bucket: 'capgo',
-    accessKey: access_key_id,
-    secretKey: access_key_secret,
-  })
+  const s3client = initR2()
 
   // Upload a file:
   return s3client.exists(fileId)
 }
 
 export const getSignedUrl = (fileId: string) => {
-  const s3client = new S3Client({
-    endPoint: `https://${accountid}.r2.cloudflarestorage.com`,
-    region: 'us-east-1',
-    bucket: 'capgo',
-    accessKey: access_key_id,
-    secretKey: access_key_secret,
-  })
+  const s3client = initR2()
   return s3client.getPresignedUrl('GET', fileId, {
     expirySeconds: 120,
   })
