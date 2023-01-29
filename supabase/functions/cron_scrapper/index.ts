10605ae4c117a4cf747400ca47693ff052669cf3
Martin Donadieu <martindonadieu@gmail.com>
2023-01-29 15:56:54 +0800

    feat: add get top app random category

--- supabase/functions/cron_scrapper/index.ts
+++ supabase/functions/cron_scrapper/index.ts
@@ -3,6 +3,63 @@ import axios from 'https://deno.land/x/axiod@0.26.2/mod.ts'
 import { supabaseAdmin } from '../_utils/supabase.ts'
 import { getEnv, sendRes } from '../_utils/utils.ts'
 
+const categories = [
+  'APPLICATION',
+  'ANDROID_WEAR',
+  'ART_AND_DESIGN',
+  'AUTO_AND_VEHICLES',
+  'BEAUTY',
+  'BOOKS_AND_REFERENCE',
+  'BUSINESS',
+  'COMICS',
+  'COMMUNICATION',
+  'DATING',
+  'EDUCATION',
+  'ENTERTAINMENT',
+  'EVENTS',
+  'FINANCE',
+  'FOOD_AND_DRINK',
+  'HEALTH_AND_FITNESS',
+  'HOUSE_AND_HOME',
+  'LIBRARIES_AND_DEMO',
+  'LIFESTYLE',
+  'MAPS_AND_NAVIGATION',
+  'MEDICAL',
+  'MUSIC_AND_AUDIO',
+  'NEWS_AND_MAGAZINES',
+  'PARENTING',
+  'PERSONALIZATION',
+  'PHOTOGRAPHY',
+  'PRODUCTIVITY',
+  'SHOPPING',
+  'SOCIAL',
+  'SPORTS',
+  'TOOLS',
+  'TRAVEL_AND_LOCAL',
+  'VIDEO_PLAYERS',
+  'WATCH_FACE',
+  'WEATHER',
+  'GAME',
+  'GAME_ACTION',
+  'GAME_ADVENTURE',
+  'GAME_ARCADE',
+  'GAME_BOARD',
+  'GAME_CARD',
+  'GAME_CASINO',
+  'GAME_CASUAL',
+  'GAME_EDUCATIONAL',
+  'GAME_MUSIC',
+  'GAME_PUZZLE',
+  'GAME_RACING',
+  'GAME_ROLE_PLAYING',
+  'GAME_SIMULATION',
+  'GAME_SPORTS',
+  'GAME_STRATEGY',
+  'GAME_TRIVIA',
+  'GAME_WORD',
+  'FAMILY',
+]
+
 serve(async (event: Request) => {
   const API_SECRET = getEnv('API_SECRET')
   const authorizationSecret = event.headers.get('apisecret')
@@ -35,9 +92,12 @@ serve(async (event: Request) => {
       .order('created_at', { ascending: true })
 
     const all = []
+    const randomCategory = categories[Math.floor(Math.random() * categories.length)]
     console.log('appsToGetCapacitor', appsToGetCapacitor?.length || 0)
     console.log('appsToGetInfo', appsToGetInfo?.length || 0)
     console.log('appsToGetSimilar', appsToGetSimilar?.length || 0)
+    console.log('randomCategory', randomCategory)
+    all.push(axios.get(`https://netlify.capgo.app/get_top_apk-background?category=${randomCategory}`))
     for (const app of (appsToGetCapacitor || []))
       all.push(axios.get(`https://netlify.capgo.app/get_capacitor-background?appId=${app.app_id}`))
 
