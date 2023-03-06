e03c2e4471e2e3bcfccb1ca0e2368eedeb35311e
Martin Donadieu <martindonadieu@gmail.com>
2023-03-06 15:03:13 +0100

    fix: store top

--- supabase/functions/store_top/index.ts
+++ supabase/functions/store_top/index.ts
@@ -1,17 +1,48 @@
 import { serve } from 'https://deno.land/std@0.177.0/http/server.ts'
 import { methodJson, sendRes } from '../_utils/utils.ts'
 import { supabaseAdmin } from '../_utils/supabase.ts'
-import type { AppStats, BaseHeaders } from '../_utils/types.ts'
+import type { BaseHeaders } from '../_utils/types.ts'
 
-const main = async (url: URL, headers: BaseHeaders, method: string, body: AppStats) => {
+interface TopStore {
+  mode?: 'capacitor' | 'cordova' | 'flutter' | 'reactNative'
+}
+
+const main = async (url: URL, headers: BaseHeaders, method: string, body: TopStore) => {
   try {
     console.log('body', body)
-    const { data, error } = await supabaseAdmin()
+    const req = supabaseAdmin()
       .from('store_apps')
       .select('url, title, icon, summary, installs, category')
       .order('installs', { ascending: false })
-      .eq('capacitor', true)
       .limit(100)
+
+    if (body.mode === 'cordova') {
+      req.eq('react_native', true)
+      req.eq('flutter', false)
+      req.eq('cordova', true)
+      req.eq('capacitor', false)
+    }
+    else if (body.mode === 'flutter') {
+      req.eq('react_native', false)
+      req.eq('flutter', true)
+      req.eq('cordova', false)
+      req.eq('capacitor', false)
+    }
+    else if (body.mode === 'reactNative') {
+      req.eq('react_native', true)
+      req.eq('flutter', false)
+      req.eq('cordova', false)
+      req.eq('capacitor', false)
+    }
+    else {
+      req.eq('react_native', false)
+      req.eq('flutter', false)
+      req.eq('cordova', true)
+      req.eq('capacitor', true)
+    }
+
+    const { data, error } = await req
+
     if (data && !error)
       return sendRes({ apps: data || [] })
     console.log('Supabase error:', error)
