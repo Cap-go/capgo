5081d0c01c15eeb8d06d629889998e0e900a9d85
Martin Donadieu <martindonadieu@gmail.com>
2023-03-25 17:02:09 +0200

    fix: plan page design

--- src/pages/dashboard/settings/Plans.vue
+++ src/pages/dashboard/settings/Plans.vue
@@ -1,9 +1,6 @@
 <script setup lang="ts">
 import { useI18n } from 'vue-i18n'
-import {
-  kSegmented,
-  kSegmentedButton,
-} from 'konsta/vue'
+
 import { computed, ref, watch, watchEffect } from 'vue'
 import { useRoute } from 'vue-router'
 import { openCheckout } from '~/services/stripe'
@@ -162,40 +159,67 @@ const hightLights = computed<Stat[]>(() => ([
           <StatsBar :mini="true" :stats="hightLights" />
         </template>
       </BlurBg>
-      <div class="mx-auto mt-4 md:w-1/4">
-        <k-segmented outline class="dark:text-gray-300 dark:bg-black">
-          <k-segmented-button
-            class="h-10"
-            :active="segmentVal === 'm'"
-            @click="() => (segmentVal = 'm')"
+      <div class="flex items-center justify-center mt-8 space-x-6 sm:mt-12">
+        <div class="flex items-center" @click="segmentVal = 'm'">
+          <input
+            id="monthly" type="radio" name="pricing-plans"
+            class="w-4 h-4 text-blue-600 border border-gray-200 focus:ring-1 focus:outline-none focus:ring-blue-600"
+            :checked="segmentVal === 'm'"
           >
-            {{ t('monthly') }}
-          </k-segmented-button>
-          <k-segmented-button
-            class="h-10"
-            :active="segmentVal === 'y'"
-            @click="() => (segmentVal = 'y')"
+          <label for="monthly" class="block ml-3 text-sm font-medium text-gray-200 sm:text-base">
+            Monthly Plan
+          </label>
+        </div>
+
+        <div class="flex items-center" @click="segmentVal = 'y'">
+          <input
+            id="yearly" type="radio" name="pricing-plans"
+            class="w-4 h-4 text-blue-600 border border-gray-200 focus:ring-1 focus:outline-none focus:ring-blue-600"
+            :checked="segmentVal === 'y'"
           >
+          <label for="yearly" class="block ml-3 text-sm font-medium text-gray-200 sm:text-base">
             {{ t('yearly') }}
-          </k-segmented-button>
-        </k-segmented>
+          </label>
+          <span class="ml-1 text-sm font-medium text-blue-600">
+            ({{ t('save') }} 20%)
+          </span>
+        </div>
       </div>
-      <div class="mt-6 space-y-4 sm:space-y-0 sm:grid sm:grid-cols-2 sm:gap-6 lg:max-w-4xl lg:mx-auto xl:max-w-none xl:mx-0 xl:grid-cols-4">
-        <div v-for="p in displayPlans" :key="p.id" class="border border-gray-200 divide-y divide-gray-200 rounded-lg shadow-sm" :class="p.name === currentPlan?.name ? 'border-4 border-muted-blue-600' : ''">
-          <div class="p-6">
+      <div class="mt-12 space-y-4 sm:space-y-0 sm:grid sm:grid-cols-2 sm:gap-6 lg:max-w-4xl lg:mx-auto xl:max-w-none xl:mx-0 xl:grid-cols-4">
+        <div v-for="p in displayPlans" :key="p.id" class="relative border border-gray-200 divide-y divide-gray-200 rounded-lg shadow-sm" :class="p.name === currentPlan?.name ? 'border-4 border-muted-blue-600' : ''">
+          <div v-if="currentPlanSuggest?.name === p.name && currentPlan?.name !== p.name" class="absolute top-0 right-0 flex items-start -mt-8">
+            <svg
+              class="w-auto h-16 text-blue-600" viewBox="0 0 83 64" fill="currentColor"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M4.27758 62.7565C4.52847 63.5461 5.37189 63.9827 6.16141 63.7318L19.0274 59.6434C19.817 59.3925 20.2536 58.5491 20.0027 57.7595C19.7518 56.97 18.9084 56.5334 18.1189 56.7842L6.68242 60.4184L3.04824 48.982C2.79735 48.1924 1.95394 47.7558 1.16441 48.0067C0.374889 48.2576 -0.0617613 49.101 0.189127 49.8905L4.27758 62.7565ZM13.4871 47.8215L12.229 47.0047L13.4871 47.8215ZM39.0978 20.5925L38.1792 19.4067L39.0978 20.5925ZM7.03921 62.9919C8.03518 61.0681 13.1417 51.1083 14.7453 48.6383L12.229 47.0047C10.5197 49.6376 5.30689 59.8127 4.37507 61.6126L7.03921 62.9919ZM14.7453 48.6383C22.0755 37.3475 29.8244 29.6738 40.0164 21.7784L38.1792 19.4067C27.7862 27.4579 19.7827 35.3698 12.229 47.0047L14.7453 48.6383ZM40.0164 21.7784C52.6582 11.9851 67.634 7.57932 82.2576 3.44342L81.4412 0.556653C66.8756 4.67614 51.3456 9.20709 38.1792 19.4067L40.0164 21.7784Z"
+              />
+            </svg>
+            <span class="ml-2 -mt-2 text-sm font-semibold text-blue-600">
+              {{ t('recommended') }}
+            </span>
+          </div>
+          <div class="p-6 border-none">
             <h2 class="text-lg font-medium leading-6 text-gray-900 dark:text-white">
               {{ p.name }}
             </h2>
             <p class="mt-4 text-sm text-gray-500 dark:text-gray-100">
               {{ t(convertKey(p.description)) }}
+              {{ t('plan-solo') }}
             </p>
             <p class="mt-8">
               <span class="text-4xl font-extrabold text-gray-900 dark:text-white">€{{ getPrice(p, segmentVal) }}</span>
               <span class="text-base font-medium text-gray-500 dark:text-gray-100">/{{ isYearly ? 'yr' : 'mo' }}</span>
             </p>
             <span v-if="isYearly" class="ml-3 text-md font-semibold text-white px-1.5 bg-emerald-500 rounded-full"> {{ getSale(p) }} </span>
-            <button v-if="p.stripe_id !== 'free'" class="block w-full py-2 mt-8 text-sm font-semibold text-center text-white bg-gray-800 border border-gray-800 rounded-md dark:bg-white dark:text-black hover:bg-gray-900 dark:hover:bg-gray-200 disabled:cursor-not-allowed disabled:bg-gray-500 disabled:dark:bg-gray-400" :disabled="currentPlan?.name === p.name" @click="openChangePlan(p.stripe_id)">
-              {{ t('plan-buy') }} {{ p.name }}
+            <button
+              v-if="p.stripe_id !== 'free'"
+              :class="{ 'bg-blue-600 hover:bg-blue-700 focus:ring-blue-700': currentPlanSuggest?.name === p.name, 'bg-gray-400 dark:bg-white dark:text-black hover:bg-gray-500 focus:ring-gray-500': currentPlanSuggest?.name !== p.name, 'cursor-not-allowed bg-gray-500 dark:bg-gray-400': currentPlan?.name === p.name }"
+              class="block w-full py-2 mt-8 text-sm font-semibold text-center text-white border border-gray-800 rounded-md"
+              :disabled="currentPlan?.name === p.name" @click="openChangePlan(p.stripe_id)"
+            >
+              {{ t('plan-upgrade') }}
             </button>
           </div>
           <div class="px-6 pt-6 pb-8">
