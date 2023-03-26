74a0028f6ab92e4f5fbacdbdf9d1e9f9a97e446e
Martin Donadieu <martindonadieu@gmail.com>
2023-03-26 05:00:14 +0300

    fix: cloudflare

--- cloudflare_workers/netlify-egde.js
+++ cloudflare_workers/netlify-egde.js
@@ -3,7 +3,7 @@ const transform = (url, request) => {
   const urlNew = new URL(url)
   const end = urlNew.pathname.split('/').pop()
   urlNew.hostname = baseNetlify
-  urlNew.pathname = `/api/${end}`
+  urlNew.pathname = `/api-egde/${end}`
   // console.log('url2', url, urlNew.toString(), await request.json())
   // request.json().then((res) => console.log('res', res))
   const headers = {
