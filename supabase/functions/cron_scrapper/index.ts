60684367e3e7c6846a16910df06fa220fa790b40
Martin Donadieu <martindonadieu@gmail.com>
2023-04-05 08:30:47 +0300

    fix: localhost path

--- supabase/functions/cron_scrapper/index.ts
+++ supabase/functions/cron_scrapper/index.ts
@@ -11,7 +11,7 @@ const toGetSimilar = 500
 
 const main = async (url: URL, headers: BaseHeaders, method: string, body: any) => {
   const DEV_FUNCTIONS = getEnv('DEV_FUNCTIONS')
-  const baseApi = DEV_FUNCTIONS ? 'http://localhost:8881' : 'https://netlify.capgo.app'
+  const baseApi = DEV_FUNCTIONS ? 'http://localhost:54321/functions/v1' : 'https://netlify.capgo.app'
   const API_SECRET = getEnv('API_SECRET')
   const authorizationSecret = headers.apisecret
   if (!authorizationSecret)
