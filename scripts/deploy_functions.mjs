03d5db1a410ba9f1801bb57492292521b747e21b
Martin Donadieu <martindonadieu@gmail.com>
2023-04-05 04:20:42 +0300

    fix: use latest CLI to install

--- scripts/deploy_functions.mjs
+++ scripts/deploy_functions.mjs
@@ -1,7 +1,7 @@
-import { existsSync, readdirSync } from 'fs'
-import { promisify } from 'util'
-import { exec as execCb } from 'child_process'
-import { exit } from 'process'
+import { existsSync, readdirSync } from 'node:fs'
+import { promisify } from 'node:util'
+import { exec as execCb } from 'node:child_process'
+import { exit } from 'node:process'
 import { outputFile } from 'fs-extra'
 import { supa_url } from './utils.mjs'
 
@@ -16,36 +16,36 @@ try {
   console.log('projectRef', projectRef)
   await outputFile('./supabase/.temp/project-ref', projectRef)
   // for in folders
-  // const all = []
+  const all = []
   for (const folder of folders) {
-    const fileNoJWT = `./supabase/functions/${folder}/.no_verify_jwt`
+    // const fileNoJWT = `./supabase/functions/${folder}/.no_verify_jwt`
     const fileNoDeploy = `./supabase/functions/${folder}/.no_deploy`
-    let command = `supabase functions deploy ${folder}`
-    let no_verify_jwt = false
-    if (existsSync(fileNoJWT)) {
-      command += ' --no-verify-jwt'
-      no_verify_jwt = true
-    }
+    const command = `supabase functions deploy ${folder}`
+    const no_verify_jwt = false
+    // if (existsSync(fileNoJWT)) {
+    //   command += ' --no-verify-jwt'
+    //   no_verify_jwt = true
+    // }
     if (!existsSync(fileNoDeploy)) {
       console.log(`Upload ${folder}${no_verify_jwt ? ' no_verify_jwt' : ''}`)
-      // all.push(exec(command).then((r) => {
-      await exec(command).then((r) => {
-        if (r.stderr) {
+      all.push(exec(command).then((r) => {
+      // await exec(command).then((r) => {
+        if (r.stderr && r.stderr !== 'Version 1.30.3 is already installed\n') {
           console.error(folder, r.stderr)
           exit(1)
         }
         console.log(`Done ${folder} ✅`)
         return r
-      })
-    // }))
+      // })
+      }))
     }
     else {
       console.log(`Ignored ${folder} ⏭`)
     }
   }
-  // await Promise.all(all)
+  await Promise.all(all)
 }
 catch (e) {
-  console.error(e) // should contain code (exit code) and signal (that caused the termination).
+  console.error('error', e) // should contain code (exit code) and signal (that caused the termination).
   exit(1)
 }
