b93605e3abd980aaed12d0fa8234c531a872bc26
Martin Donadieu <martindonadieu@gmail.com>
2023-04-20 17:53:07 +0100

    fix: bg issue

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@ const Dialog = defineAsyncComponent(() => import('~/components/Dialog.vue'))
 
 <template>
   <kProvider theme="ios">
-    <div class="h-full overflow-hidden k-ios">
+    <div class="h-full overflow-hidden k-ios bg-slate-100 text-slate-600 dark:bg-slate-900 dark:text-slate-300">
       <RouterView class="h-full overflow-hidden" />
       <ActionSheet />
       <Toast />
