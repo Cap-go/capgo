661dbc6c4f7a3eb3d47f56d6bfb046cb1e549ce2
Martin Donadieu <martindonadieu@gmail.com>
2023-07-26 15:02:02 +0100

    feat: add on_user_delete

---
+++ supabase/functions/on_user_delete/index.ts
@@ -0,0 +1,41 @@
+import { serve } from 'https://deno.land/std@0.188.0/http/server.ts'
+import type { InsertPayload } from '../_utils/supabase.ts'
+import type { Database } from '../_utils/supabase.types.ts'
+import { getEnv, sendRes } from '../_utils/utils.ts'
+
+serve(async (event: Request) => {
+  const API_SECRET = getEnv('API_SECRET')
+  const authorizationSecret = event.headers.get('apisecret')
+  if (!authorizationSecret || !API_SECRET || authorizationSecret !== API_SECRET)
+    return sendRes({ message: 'Fail Authorization' }, 400)
+
+  try {
+    const table: keyof Database['public']['Tables'] = 'deleted_account'
+    const body = (await event.json()) as InsertPayload<typeof table>
+    if (body.table !== table) {
+      console.log(`Not ${table}`)
+      return sendRes({ message: `Not ${table}` }, 200)
+    }
+    if (body.type !== 'INSERT') {
+      console.log('Not INSERT')
+      return sendRes({ message: 'Not INSERT' }, 200)
+    }
+    const record = body.record
+    console.log('record', record)
+    if (record.id) {
+      console.log('No id')
+      return sendRes()
+    }
+    // delete all user org
+    // delete all apps from org
+    // delete stripe customer
+    return sendRes()
+  }
+  catch (e) {
+    console.log('Error', e)
+    return sendRes({
+      status: 'Error unknow',
+      error: JSON.stringify(e),
+    }, 500)
+  }
+})
