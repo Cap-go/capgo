aa0894bdb0449c3e33a3b0f1b9ab9546a2596f46
Martin Donadieu <martindonadieu@gmail.com>
2023-08-03 12:20:00 +0100

    fix: load balancer use backend is sequence

--- cloudflare_workers/loadbanlancer.js
+++ cloudflare_workers/loadbanlancer.js
@@ -1,13 +1,8 @@
-const HOSTS = [
-  'xvwzpoazmxkqosrdewyv.functions.supabase.co',
-  'web.capgo.app',
-]
+const MAINHOST = 'xvwzpoazmxkqosrdewyv.functions.supabase.co'
+const BACKUPHOST = 'netlifyedge.capgo.app'
+const BACKUPHOSTNODE = 'netlify.capgo.app'
 
-const getRandomInt = (max) => {
-  return Math.floor(Math.random() * max)
-}
-
-const fetchWithTimeout = async (resource, options = {}) => {
+async function fetchWithTimeout(resource, options = {}) {
   const { timeout = 5000 } = options
 
   const controller = new AbortController()
@@ -20,36 +15,39 @@ const fetchWithTimeout = async (resource, options = {}) => {
   return response
 }
 
-const handleRequest = async (request) => {
-  // Randomly pick the next host
-  const primary = getRandomInt(HOSTS.length)
+async function handleRequest(request) {
   const primaryUrl = new URL(request.url)
-  primaryUrl.hostname = HOSTS[primary]
+  primaryUrl.hostname = MAINHOST
 
-  // Fallback if there is no response within timeout
-  let backup
-  do {
-    // Naive solution to pick a backup host
-    backup = getRandomInt(HOSTS.length)
-  } while (backup === primary)
-
-  const backupUrl = new URL(request.url)
-  backupUrl.hostname = HOSTS[backup]
-
-  // console.log("yoyo2", primary, primaryUrl.toString(), backupUrl.toString())
   let res
   try {
     res = await fetchWithTimeout(primaryUrl, request.clone())
   }
   catch (err) {
     console.log('err', primaryUrl, err)
-    res = await fetch(backupUrl, request.clone())
+    // try second host
+    const backupUrl = new URL(request.url)
+    backupUrl.hostname = BACKUPHOST
+    try {
+      res = await fetch(backupUrl, request.clone())
+    }
+    catch (err) {
+      console.log('err', backupUrl, err)
+      // try third host
+      const backup2Url = new URL(request.url)
+      backup2Url.hostname = BACKUPHOSTNODE
+      try {
+        res = await fetch(backup2Url, request.clone())
+      }
+      catch (err) {
+        console.log('err', backup2Url, err)
+        return err
+      }
+    }
   }
-  // console.log("yoyo3", res)
   return res
 }
 
 addEventListener('fetch', (event) => {
-  // console.log("yoyo")
   event.respondWith(handleRequest(event.request))
 })
