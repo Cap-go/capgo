829c2943f5da1d0fce9eb4c8649531b08884c97c
Martin Donadieu <martindonadieu@gmail.com>
2023-08-11 23:27:32 +0200

    fix: use base schema instead of many migrations

--- supabase/migrations/20230726140052_on_user_delete.sql
+++
@@ -1,5 +0,0 @@
---
--- Name: users on_user_create; Type: TRIGGER; Schema: public; Owner: supabase_admin
---
-
-CREATE TRIGGER on_user_create AFTER INSERT ON public.users FOR EACH ROW EXECUTE FUNCTION supabase_functions.http_request('http://localhost:54321/functions/v1/on_user_delete', 'POST', '{"Content-type":"application/json","apisecret":"Y3p63TMDGNTHTze6MchBM7tPmB5"}', '{}', '1000');
