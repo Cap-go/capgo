3f9da867659102a667aefda5e9b48a331c1ebb56
Peter Kompasz <kompaszbence@gmail.com>
2023-08-20 20:42:59 +0300

    feat: redirect icon

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -65,12 +65,14 @@ const tabs = ref<Tab[]>([
     icon: shallowRef(IconDoc),
     key: '#',
     onClick: () => window.open('https://docs.capgo.app', '_blank'),
+    redirect: true,
   },
   {
     label: t('discord'),
     icon: shallowRef(IconDiscord),
     key: '#',
     onClick: () => window.open('https://discord.gg/VnYRvBfgA6', '_blank'),
+    redirect: true,
   },
 ])
 </script>
@@ -111,6 +113,9 @@ const tabs = ref<Tab[]>([
                   <div class="flex items-center">
                     <component :is="tab.icon" class="w-6 h-6 fill-current" :class="{ 'text-blue-600': isTabActive(tab.key), 'text-slate-400': !isTabActive(tab.key) }" />
                     <span class="ml-3 text-sm font-medium duration-200 lg:sidebar-expanded:opacity-100 2xl:opacity-100 lg:opacity-0" :class="{ 'text-blue-600': isTabActive(tab.key), 'text-slate-400': !isTabActive(tab.key) }">{{ tab.label }}</span>
+                    <svg v-if="tab.redirect" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
+                    </svg>
                   </div>
                 </div>
               </button>
