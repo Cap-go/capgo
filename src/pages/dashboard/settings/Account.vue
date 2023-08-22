4f69d217a90d0b46952f6944f299e1ce8aef098c
neo773 <62795688+neo773@users.noreply.github.com>
2023-08-22 15:38:07 +0530

    Improve language selector

--- src/pages/dashboard/settings/Account.vue
+++ src/pages/dashboard/settings/Account.vue
@@ -16,7 +16,7 @@ import { deleteUser, useSupabase } from '~/services/supabase'
 import type { Database } from '~/types/supabase.types'
 import { useDisplayStore } from '~/stores/display'
 import IconVersion from '~icons/radix-icons/update'
-import { availableLocales, i18n, loadLanguageAsync } from '~/modules/i18n'
+import { availableLocales, i18n, loadLanguageAsync, languages } from '~/modules/i18n'
 import { iconEmail, iconName } from '~/services/icons'
 
 const version = import.meta.env.VITE_APP_VERSION
@@ -333,13 +333,13 @@ onMounted(() => {
           </p>
           <div class="md:ml-6">
             <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
-              {{ getEmoji(i18n.global.locale.value) }} <svg class="w-4 h-4 ml-2" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" /></svg>
+              {{ getEmoji(i18n.global.locale.value) }} {{languages[i18n.global.locale.value as keyof typeof languages] }} <svg class="w-4 h-4 ml-2" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" /></svg>
             </button>
             <!-- Dropdown menu -->
-            <div id="dropdown" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
+            <div id="dropdown" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 h-72 overflow-y-scroll">
               <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
                 <li v-for="locale in availableLocales" :key="locale" @click="loadLanguageAsync(locale)">
-                  <span class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">{{ getEmoji(locale) }}</span>
+                  <span class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">{{ getEmoji(locale) }} {{languages[locale as keyof typeof languages] }}</span>
                 </li>
               </ul>
             </div>
