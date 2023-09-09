aa6080f3eafbe925ccfb52f4dd16d223f7a63a63
neo773 <62795688+neo773@users.noreply.github.com>
2023-09-09 15:48:04 +0530

    fix logic and lint

--- src/pages/dashboard/settings/Plans.vue
+++ src/pages/dashboard/settings/Plans.vue
@@ -4,6 +4,7 @@ import { computed, ref, watch, watchEffect } from 'vue'
 import { useRoute } from 'vue-router'
 import { toast } from 'vue-sonner'
 import { Capacitor } from '@capacitor/core'
+import dayjs from 'dayjs'
 import { openCheckout } from '~/services/stripe'
 import { useMainStore } from '~/stores/main'
 import { findBestPlan, getCurrentPlanName, getPlanUsagePercent, getPlans, getTotalStats } from '~/services/supabase'
@@ -11,7 +12,6 @@ import { useLogSnag } from '~/services/logsnag'
 import { openMessenger } from '~/services/chatwoot'
 import type { Database } from '~/types/supabase.types'
 import type { Stat } from '~/components/comp_def'
-import dayjs from 'dayjs'
 
 function openSupport() {
   openMessenger()
@@ -147,31 +147,38 @@ const hightLights = computed<Stat[]>(() => ([
 
 <template>
   <div v-if="!isLoading" class="h-full bg-white max-h-fit dark:bg-gray-800">
-
-      <div class="px-4 pt-6 mx-auto max-w-7xl lg:px-8 sm:px-6" v-if="!displayPlans && currentPlan?.name !== 'free'">
-        <div class="sm:align-center sm:flex sm:flex-col">
-          <h1 class="text-5xl font-extrabold text-gray-900 sm:text-center dark:text-white">
-            {{ t('billing') }}
-          </h1>
-          <div   class="flex justify-center my-8" >
-
-              <div class="bg-gray-100 p-4 rounded-lg shadow-md w-[500px]">
-                <h2 class="text-2xl font-bold text-gray-900">Current Billing Cycle</h2>
-                <div class="flex justify-between mt-2">
-                  <div>
-                    <h3 class="text-lg font-semibold text-gray-700">From</h3>
-                    <p class="text-gray-600">{{ dayjs(main.cycleInfo?.subscription_anchor_end).format('MMMM D YYYY') }}</p>
-                  </div>
-                  <div>
-                    <h3 class="text-lg font-semibold text-gray-700">To</h3>
-                    <p class="text-gray-600">{{ dayjs(main.cycleInfo?.subscription_anchor_start).format('MMMM D YYYY') }}</p>
-                  </div>
-                </div>
+    <div v-if="currentPlan?.name !== 'free'" class="px-4 pt-6 mx-auto max-w-7xl lg:px-8 sm:px-6">
+      <div class="sm:align-center sm:flex sm:flex-col">
+        <h1 class="text-5xl font-extrabold text-gray-900 sm:text-center dark:text-white">
+          {{ t('billing') }}
+        </h1>
+        <div class="flex justify-center my-8">
+          <div class="bg-gray-100 p-4 rounded-lg shadow-md w-[500px]">
+            <h2 class="text-2xl font-bold text-gray-900">
+              Current Billing Cycle
+            </h2>
+            <div class="flex justify-between mt-2">
+              <div>
+                <h3 class="text-lg font-semibold text-gray-700">
+                  From
+                </h3>
+                <p class="text-gray-600">
+                  {{ dayjs(main.cycleInfo?.subscription_anchor_end).format('MMMM D YYYY') }}
+                </p>
               </div>
+              <div>
+                <h3 class="text-lg font-semibold text-gray-700">
+                  To
+                </h3>
+                <p class="text-gray-600">
+                  {{ dayjs(main.cycleInfo?.subscription_anchor_start).format('MMMM D YYYY') }}
+                </p>
               </div>
-        </div>
+            </div>
           </div>
-
+        </div>
+      </div>
+    </div>
 
     <div class="px-4 pt-6 mx-auto max-w-7xl lg:px-8 sm:px-6">
       <div class="sm:align-center sm:flex sm:flex-col">
