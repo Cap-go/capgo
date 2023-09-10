93dcdd7c791f33d74b20051cf3df070ac97cbc49
neo773 <62795688+neo773@users.noreply.github.com>
2023-09-10 17:23:27 +0530

    tweak

--- playwright.config.ts
+++ playwright.config.ts
@@ -41,28 +41,17 @@ export default defineConfig({
   reporter: [
     ["list", { printSteps: true }],
     ["github"],
+    ["html", { outputFolder: "./test-results/reports/playwright-html-report", open: "never" }],
   ],
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
     headless: headless,
   },
   webServer: webServer,
-  /* Configure projects for major browsers */
   projects: [
     {
       name: "chromium",
       use: { ...devices["Desktop Chrome"] },
     }
   ],
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
 });
