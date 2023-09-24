cc80c5866154380533b90395fe547fa96f951041
WcaleNieWolny <isupermichael007@gmail.com>
2023-09-24 20:05:02 +0200

    Fix updates test repetition

--- tests_backend/tests/backend/updates_test.ts
+++ tests_backend/tests/backend/updates_test.ts
@@ -14,7 +14,7 @@ export function getTest(): RunnableTest {
       {
         name: 'Test updates endpoint (big)',
         test: testUpdateEndpoint,
-        execute: 1,
+        execute: 3,
       },
     ],
   }
