9112aee80c88082fc7a96dd8618332d9c86aebde
WcaleNieWolny <isupermichael007@gmail.com>
2023-10-02 06:31:46 +0200

    More tests

--- tests/selectable_disallow.spec.ts
+++ tests/selectable_disallow.spec.ts
@@ -13,5 +13,19 @@ test('test selectable disallow no metadata warning', async ({ page }) => {
   // Checks if the warning triggered
   await expect(page.locator('.k-ios > section:nth-child(4) > ol:nth-child(1) > li:nth-child(1) > div:nth-child(3) > div:nth-child(1)')).toContainText('Minimal update version')
 
-  await page.waitForTimeout(10000)
+  // Back to information page
+  await page.click('li.mr-2:nth-child(1) > button:nth-child(1)')
+
+  // Check if the 'minimal update version' is present
+  await expect(page.locator('div.px-4:nth-child(3) > dt:nth-child(1)')).toContainText('Minimal update version')
+  await expect(page.locator('div.px-4:nth-child(3) > dd:nth-child(2) > div:nth-child(1) > span:nth-child(1)')).toContainText('Undefined')
+
+  // Go to the channel bundle
+  await page.click('.cursor-pointer > div:nth-child(1) > span:nth-child(1)')
+
+  // Check if the 'Minimal update version' is present
+  await expect(page.locator('div.px-4:nth-child(6) > dt:nth-child(1)')).toContainText('Minimal update version')
+
+  // Check if the input is empty
+  // input.block
 })
