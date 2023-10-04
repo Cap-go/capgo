926c5f09da079922a885b61136112190274718ff
WcaleNieWolny <isupermichael007@gmail.com>
2023-10-04 19:24:26 +0200

    test cli

--- tests/bundle.spec.ts
+++ tests/bundle.spec.ts
@@ -4,7 +4,8 @@ const BASE_URL = 'http://localhost:5173'
 
 test('test bundle', async ({ page }) => {
   // Go to the production channel
-  page.goto(`${BASE_URL}/app/p/com--demo--app/channel/22`)
+  await page.goto(`${BASE_URL}/app/p/com--demo--app/channel/22`)
+
   // eslint-disable-next-line n/prefer-global/process
   await expect(page.locator('.cursor-pointer > div:nth-child(1) > span:nth-child(1)')).toHaveText(process.env.BUNDLE!, { timeout: 60_000 })
 })
