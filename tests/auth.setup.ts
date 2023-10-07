a0aa7e11c577b508d7968acaf2aaf80d0ea202c0
WcaleNieWolny <isupermichael007@gmail.com>
2023-10-07 08:03:39 +0200

    Remove waiting

--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -5,28 +5,6 @@ const authFile1 = 'playwright/.auth/user1.json'
 // const authFile2 = 'playwright/.auth/user2.json'
 
 setup('authenticate as test1', async ({ page }) => {
-  // I am sorry. This SUCS.
-  // I have no idea how to make this work in a better way.
-  // The only reason this is here is becouse the dev server takes ages to load
-  // And the request always fails until this server is loaded
-
-  // Limit the max iterations
-  let i = 200
-
-  while (true) {
-    if (i === 0)
-      throw new Error('Failed to load the dev server')
-
-    try {
-      const response = await fetch(BASE_URL)
-      break
-    }
-    catch (e) {
-      await page.waitForTimeout(1000)
-      i--
-    }
-  }
-
   await page.goto(`${BASE_URL}/`)
 
   // Fill in the username and password fields
