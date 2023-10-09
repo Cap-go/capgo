f9251ca574b994373dc0e6a3b3945ec1b29fece4
keremecon <134702704+kremedev@users.noreply.github.com>
2023-10-09 20:46:31 +0300

    how???

--- tests_backend/tests/backend/updates_test.ts
+++ tests_backend/tests/backend/updates_test.ts
@@ -255,7 +255,7 @@ async function testTwoChannels(_backendBaseUrl: URL, supabase: SupabaseType) {
   const { error: changeSecondChannelError } = await supabase.from('channels').update({ android: true }).eq('id', 24)
   assert(changeSecondChannelError === null, `Supabase change production channel (ios) error ${JSON.stringify(changeSecondChannelError)} is not null`)
   try {
-    await delay(5000)
+    await delay(3000)
     const { data: prodChannel, error: getSecondChannelError } = await supabase
       .from('channels')
       .select('*')
@@ -278,7 +278,7 @@ async function testTwoChannels(_backendBaseUrl: URL, supabase: SupabaseType) {
   const { error: changeSecondChannelError2 } = await supabase.from('channels').update({ android: false }).eq('id', 22)
   assert(changeSecondChannelError2 === null, `Supabase change production channel (ios) error ${JSON.stringify(changeSecondChannelError2)} is not null`)
   try {
-    await delay(5000)
+    await delay(3000)
     const { data: secondChannel, error: getSecondChannelError } = await supabase
       .from('channels')
       .select('*')
@@ -295,13 +295,14 @@ async function testTwoChannels(_backendBaseUrl: URL, supabase: SupabaseType) {
 
     const { error: changeSecondChannelError } = await supabase.from('channels').update({ public: true }).eq('id', 24)
     assert(changeSecondChannelError === null, `Supabase change production channel (ios) error ${JSON.stringify(changeSecondChannelError)} is not null`)
+    await delay(1500)
   }
 
   // We update two_default channel iOS: false then check if production channel is still public or not
   const { error: changeSecondChannelError3 } = await supabase.from('channels').update({ ios: false }).eq('id', 24)
   assert(changeSecondChannelError3 === null, `Supabase change production channel (ios) error ${JSON.stringify(changeSecondChannelError3)} is not null`)
   try {
-    await delay(5000)
+    await delay(3000)
     const { data: prodChannel, error: getSecondChannelError } = await supabase
       .from('channels')
       .select('*')
