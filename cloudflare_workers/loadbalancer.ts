a2c990bfaad6325b00ed67c29eeccd7cb7c12722
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2023-10-14 01:23:12 +0530

    Update loadbalancer.ts

--- cloudflare_workers/loadbalancer.ts
+++ cloudflare_workers/loadbalancer.ts
@@ -17,45 +17,58 @@ async function fetchWithTimeout(resource: RequestInfo, options: RequestOptions =
   return response
 }
 
-async function handleRequest(request: Request): Promise<Response> {
-  const primaryUrl = new URL(request.url)
-  primaryUrl.hostname = MAIN_HOST
-  let res: Response
-  try {
-    res = await fetchWithTimeout(primaryUrl.toString(), request.clone())
-  }
-  catch (err) {
-    console.log('err', primaryUrl, err)
-    // try the second host
-    const backupUrl = new URL(request.url)
-    backupUrl.hostname = BACKUP_HOST
-    const end = backupUrl.pathname.split('/').pop()
-    // https://web.capgo.app/api-egde/ok
-    backupUrl.pathname = `/api-egde/${end}`
+export default {
+  async fetch(request: Request) {
+    let res: Response
+    const primaryUrl = new URL(request.url)
+    primaryUrl.hostname = MAIN_HOST
     try {
-      // Use fetchWithTimeout for the backup requests as well
-      res = await fetchWithTimeout(backupUrl.toString(), request.clone())
+      res = await fetchWithTimeout(primaryUrl.toString(), {
+        redirect: 'follow',
+        body: request.body,
+        method: request.method,
+        headers: request.headers,
+      })
     }
     catch (err) {
-      console.log('err', backupUrl, err)
-      // try the third host
-      // https://web.capgo.app/api/ok
-      const backup2Url = new URL(request.url)
-      backup2Url.hostname = BACKUP_HOST
-      const end = backup2Url.pathname.split('/').pop()
-      backup2Url.pathname = `/api/${end}`
+      console.log('err', primaryUrl, err)
+      // try the second host
+      const backupUrl = new URL(request.url)
+      backupUrl.hostname = BACKUP_HOST
+      const end = backupUrl.pathname.split('/').pop()
+      // https://web.capgo.app/api-egde/ok
+      backupUrl.pathname = `/api-egde/${end}`
       try {
-        res = await fetchWithTimeout(backup2Url.toString(), request.clone())
+        // Use fetchWithTimeout for the backup requests as well
+        res = await fetchWithTimeout(backupUrl.toString(), {
+          redirect: 'follow',
+          body: request.body,
+          method: request.method,
+          headers: request.headers,
+        })
       }
       catch (err) {
-        console.log('err', backup2Url, err)
-        return new Response('Error', { status: 500 })
+        console.log('err', backupUrl, err)
+        // try the third host
+        // https://web.capgo.app/api/ok
+        const backup2Url = new URL(request.url)
+        backup2Url.hostname = BACKUP_HOST
+        const end = backup2Url.pathname.split('/').pop()
+        backup2Url.pathname = `/api/${end}`
+        try {
+          res = await fetchWithTimeout(backup2Url.toString(), {
+            redirect: 'follow',
+            body: request.body,
+            method: request.method,
+            headers: request.headers,
+          })
+        }
+        catch (err) {
+          console.log('err', backup2Url, err)
+          return new Response('Error', { status: 500 })
+        }
       }
     }
-  }
-  return res
+    return res
+  },
 }
-
-addEventListener('fetch', (event: FetchEvent) => {
-  event.respondWith(handleRequest(event.request))
-})
