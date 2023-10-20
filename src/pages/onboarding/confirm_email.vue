fd5ef79eb2a056d7954c22099fc45ff20e9615cf
Martin Donadieu <martindonadieu@gmail.com>
2023-10-20 01:51:20 +0100

    fix: lint issue

--- src/pages/onboarding/confirm_email.vue
+++ src/pages/onboarding/confirm_email.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { useI18n } from 'vue-i18n'
-import { useRoute, useRouter } from 'vue-router'
+import { useRoute } from 'vue-router'
 import { toast } from 'vue-sonner'
 import { useSupabase } from '~/services/supabase'
 
@@ -8,7 +8,7 @@ const { t } = useI18n()
 const route = useRoute()
 const supabase = useSupabase()
 
-const email = route.query.email
+const email = route.query.email as string
 
 async function resendEmail() {
   const { error } = await supabase.auth.resend({
@@ -25,11 +25,11 @@ async function resendEmail() {
 
 <template>
   <div class="h-screen">
-    <div class="h-full w-full flex items-center justify-center overflow-y-auto px-4 py-5 sm:p-6">
-      <div class="max-w-sm w-full rounded-xl bg-white shadow-lg">
+    <div class="flex items-center justify-center w-full h-full px-4 py-5 overflow-y-auto sm:p-6">
+      <div class="w-full max-w-sm bg-white shadow-lg rounded-xl">
         <div class="px-4 py-5 sm:p-6">
           <div class="text-center">
-            <svg class="mx-auto h-16 w-16 text-gray-900" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1">
+            <svg class="w-16 h-16 mx-auto text-gray-900" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1">
               <path stroke-linecap="round" stroke-linejoin="round" d="M5 13l4 4L19 7" />
             </svg>
             <p class="mt-5 text-xl font-bold text-gray-900">
@@ -38,13 +38,13 @@ async function resendEmail() {
             <p class="mt-3 text-sm font-medium text-gray-500">
               {{ t('check-email') }}
             </p>
-            <div class="flex gap-2 justify-center">
+            <div class="flex justify-center gap-2">
               <p class="text-sm font-medium text-gray-500">
                 {{ t('did-not-recive-confirm-email') }}
               </p>
               <p
                 to="/resend_email"
-                class="text-sm font-medium text-blue-600 transition-all duration-200 hover:text-blue-500 hover:underline cursor-pointer"
+                class="text-sm font-medium text-blue-600 transition-all duration-200 cursor-pointer hover:text-blue-500 hover:underline"
                 @click="resendEmail"
               >
                 {{ t('resend') }}
@@ -52,7 +52,7 @@ async function resendEmail() {
             </div>
             <div class="mt-8">
               <span
-                class="inline-flex items-center justify-center rounded-md px-6 py-3 text-sm font-semibold leading-5 text-blue-600 transition-all duration-200"
+                class="inline-flex items-center justify-center px-6 py-3 text-sm font-semibold leading-5 text-blue-600 transition-all duration-200 rounded-md"
               >
                 {{ t('thank-you-for-choosi') }}
               </span>
