9fd70fbf809495f8db47445dd53f3c973425b712
Martin Donadieu <martindonadieu@gmail.com>
2023-10-30 12:31:04 +0000

    fix: CLI check right problem

--- supabase/migrations/20230815171919_base.sql
+++ supabase/migrations/20230815171919_base.sql
@@ -637,6 +637,16 @@ Begin
 End;  
 $$;
 
+CREATE OR REPLACE FUNCTION public.is_allowed_action(apikey text, appid character varying)
+ RETURNS boolean
+ LANGUAGE plpgsql
+ SECURITY DEFINER
+AS $function$
+Begin
+  RETURN is_app_owner(get_user_id(apikey), appid);
+End;
+$function$
+
 CREATE OR REPLACE FUNCTION public.is_app_owner(appid character varying)
 RETURNS boolean
 LANGUAGE plpgsql
