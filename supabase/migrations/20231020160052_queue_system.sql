9df3ba3ca17eb3514452990cd0c16975b1ac5ce0
WcaleNieWolny <isupermichael007@gmail.com>
2023-10-31 06:04:41 +0100

    Queue part next

--- supabase/migrations/20231020160052_queue_system.sql
+++ supabase/migrations/20231020160052_queue_system.sql
@@ -2,7 +2,9 @@ CREATE TABLE job_queue (
     job_id serial PRIMARY KEY,
     job_type TEXT NOT NULL CHECK (job_type IN ('TRIGGER', 'APP_DELETE', 'APP_VERSION_DELETE', 'DEVICE_DELETE')),
     payload TEXT NOT NULL,
-    processing BOOLEAN NOT NULL DEFAULT FALSE,
+    -- Both external and function_name are required for TRIGGER jobs
+    function_type TEXT,
+    function_name TEXT,
     created_at TIMESTAMPTZ DEFAULT NOW()
 );
 
@@ -39,10 +41,18 @@ BEGIN
         FOR UPDATE SKIP LOCKED
         LOOP
             RAISE NOTICE 'Processing job_id: %, payload: %', current_job.job_id, current_job.payload;
-            PERFORM pg_sleep(20);
+
+            IF (current_job.job_type = 'TRIGGER' AND current_job.function_name IS NOT NULL) THEN
+                PERFORM http_post_helper(current_job.function_name, current_job.function_type, current_job.payload::jsonb);
+            END IF;
+
+            IF (current_job.job_type = 'APP_DELETE') THEN
+              --DELETE FROM "devices" where app_id=current_job.payload;
+              --DELETE FROM "stats" where app_id=current_job.payload;
+            END IF;
+
             -- Delete the job from the queue
             RAISE NOTICE 'Delete job_id: %, payload: %', current_job.job_id, current_job.payload;
-
             DELETE FROM job_queue WHERE job_id = current_job.job_id;
         END LOOP;
 
@@ -59,3 +69,32 @@ SELECT cron.schedule(
     '* * * * *',
     $$ SELECT process_current_jobs_if_unlocked(); $$
 );
+
+CREATE OR REPLACE FUNCTION public.trigger_http_queue_post_to_function() 
+RETURNS trigger 
+LANGUAGE plpgsql 
+SECURITY DEFINER
+AS $BODY$
+DECLARE 
+  payload jsonb;
+BEGIN 
+  -- Build the payload
+  payload := jsonb_build_object(
+    'old_record', OLD, 
+    'record', NEW, 
+    'type', TG_OP,
+    'table', TG_TABLE_NAME,
+    'schema', TG_TABLE_SCHEMA
+  );
+
+  -- Insert into job_queue
+  INSERT INTO job_queue (job_type, payload, function_name, function_type) VALUES ('TRIGGER', payload::text, TG_ARGV[0], TG_ARGV[1]);
+
+  RETURN NEW;
+END;
+$BODY$;
+
+CREATE TRIGGER on_user_update_queue
+AFTER UPDATE ON public.users 
+FOR EACH ROW 
+EXECUTE FUNCTION public.trigger_http_queue_post_to_function('demo');
