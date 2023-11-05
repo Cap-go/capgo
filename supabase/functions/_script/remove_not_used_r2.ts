57a1edead5a2c6d855209aa15ed0056f5a842fef
WcaleNieWolny <isupermichael007@gmail.com>
2023-11-05 06:55:48 +0100

    Implement script

--- supabase/functions/_script/remove_not_used_r2.ts
+++ supabase/functions/_script/remove_not_used_r2.ts
@@ -77,15 +77,16 @@ async function main() {
       }))
 
     const supabaseVersions = await supabase.from('app_versions')
-      .select('name')
+      .select('name, deleted')
       .eq('app_id', app.app_id)
+      .or('storage_provider.in.("r2-direct","r2")')
 
     if (supabaseVersions.error) {
       console.error(`App ${app.app_id} get supaabse version error`)
       throw supabaseVersions.error
     }
 
-    const supabaseVersionsMap = new Map(supabaseVersions.data.map(supabaseVersion => [supabaseVersion.name, '']))
+    const supabaseVersionsMap = new Map(supabaseVersions.data.map(supabaseVersion => [supabaseVersion.name, supabaseVersion.deleted]))
 
     return {
       app,
@@ -97,7 +98,10 @@ async function main() {
   const appWithMissingSupabaseVersion = appsWithR2AndSupabase.map((app) => {
     return {
       app: app.app,
-      missingSupabase: app.r2Version.filter(r2Ver => app.supabaseVersions.get(r2Ver.name) === undefined),
+      missingSupabase: app.r2Version.filter((r2Ver) => {
+        const supabaseVer = app.supabaseVersions.get(r2Ver.name)
+        return supabaseVer === undefined || supabaseVer === true
+      }),
     }
   }).filter(app => app.missingSupabase.length > 0)
 
@@ -106,8 +110,39 @@ async function main() {
   const tmpFolder = await Deno.makeTempDir()
   console.log(`Temp folder for downloading data: ${tmpFolder}`)
 
-  for (const app of appWithMissingSupabaseVersion)
+  for (const app of appWithMissingSupabaseVersion) {
     console.log(`Processing app ${app.app.app_id}`)
+
+    await Promise.all(app.missingSupabase.map(async (missingVersion) => {
+      console.log(`Downloading ${missingVersion.name} for app ${app.app.app_id}`)
+
+      const filePath = `${tmpFolder}/${missingVersion.orginalName}`
+      const folderPath = filePath.split('/').slice(0, -1).join('/')
+      await Deno.mkdir(folderPath, { recursive: true })
+
+      const downloadObjectResponse = await r2.getObject(missingVersion.orginalName)
+      const downloadFile = await Deno.open(`${tmpFolder}/${missingVersion.orginalName}`, { create: true, write: true })
+      if (!downloadObjectResponse.body) {
+        console.error(`Body null for download ${missingVersion.name} for app ${app.app.app_id}`)
+        Deno.exit(1)
+      }
+
+      const toPipe = downloadFile.writable
+      await downloadObjectResponse.body.pipeTo(toPipe)
+
+      // https://github.com/denoland/deno/issues/14210
+      try {
+        Deno.close(downloadFile.rid)
+      }
+      catch (_) {
+        // pass
+      }
+
+      console.log(`Downloading ${missingVersion.name} for app ${app.app.app_id}. Removing from R2`)
+      // Now delete the file from r2
+      await r2.deleteObject(missingVersion.orginalName)
+    }))
+  }
 }
 
 main()
