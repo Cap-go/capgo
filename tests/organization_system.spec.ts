34b079af6c88b2e0e18091c5c9a588a5917f91be
WcaleNieWolny <isupermichael007@gmail.com>
2023-11-09 05:06:35 +0100

    Tests part 2

--- tests/organization_system.spec.ts
+++ tests/organization_system.spec.ts
@@ -1,31 +1,72 @@
 // import type { Page } from '@playwright/test'
 import type { Page } from '@playwright/test'
 import { expect, test } from '@playwright/test'
-import { BASE_URL, beforeEachTest } from './utils'
+import { BASE_URL, beforeEachTest, expectPopout, useSupabase } from './utils'
 
 test.beforeEach(beforeEachTest)
 
-test('test organization invite', async ({ page }) => {
-  // await expect('abc'.length).toBe(3)
+test.describe('Test organization invite', () => {
+  test.beforeAll(async () => {
+    const supabase = await useSupabase()
 
-  await expect(page.locator('.space-x-3 > div:nth-child(1) > div:nth-child(1)')).toBeVisible()
-  await page.locator('#organization-picker').click()
+    const { error } = await supabase.from('org_users')
+      .delete()
+      .eq('user_id', '6f0d1a2e-59ed-4769-b9d7-4d9615b28fe5')
 
-  const allOrgs = (await page.locator('ul.py-2').all())
-  await expect(allOrgs).toHaveLength(1)
+    await expect(error).toBeNull()
+  })
 
-  await page.goto(`${BASE_URL}/dashboard/settings/organization/members`)
-  await getAllMembers(page)
+  test('test invite user to org', async ({ page }) => {
+    // await expect('abc'.length).toBe(3)
 
-  // For now we do not check name, not the point of this test
+    await expect(page.locator('.space-x-3 > div:nth-child(1) > div:nth-child(1)')).toBeVisible()
+    await page.locator('#organization-picker').click()
+
+    const allOrgs = (await page.locator('ul.py-2').all())
+    await expect(allOrgs).toHaveLength(1)
+
+    await page.goto(`${BASE_URL}/dashboard/settings/organization/members`)
+    await getAllMembers(page)
+
+    // For now we do not check name, not the point of this test
+    const members = await getAllMembers(page)
+    await expect(members).toHaveLength(1)
+
+    const member = members[0]
+    await expect(member.email).toBe('test@capgo.app')
+    await expect(member.isDeletable).toBeFalsy()
+    await expect(member.isModifiable).toBeFalsy()
+
+    // Let's invite shall we?
+    await page.click('button.text-white:nth-child(2)')
+    await expect(page.locator('div.rounded-lg:nth-child(1)')).toBeVisible()
+    await expect(page.locator('h3.text-xl')).toHaveText('Insert email of the user you want to invite')
+
+    // Type email and click "invite"
+    await page.fill('#dialog-input-field', 'test2@capgo.app')
+    await page.click('div.p-6:nth-child(3) > button:nth-child(2)')
+    await expect(page.locator('h3.text-xl')).toHaveText('Select user\'s permissions')
+
+    // Click on "read" (TODO: click on all)
+    await page.click('div.p-6:nth-child(3) > button:nth-child(2)')
+    await expectPopout(page, 'Successfully invited user to org')
+
+    // Did we invite?
+    const supabase = await useSupabase()
+    const orgName = await getOrgName(page)
+
+    // const { data, error } = await supabase.from('org_users')
+    //   .select()
+    //   .eq('')
+  })
 })
 
 async function getAllMembers(page: Page) {
+  await page.goto(`${BASE_URL}/dashboard/settings/organization/members`)
   await page.waitForTimeout(500)
 
   const userTable = await page.locator('dl.divide-y')
   const userTableDivs = await userTable.all()
-  console.log('a')
 
   const members = await Promise.all(userTableDivs.map(async (el) => {
     const email = await el.locator('div:nth-child(2)').innerText()
@@ -34,18 +75,12 @@ async function getAllMembers(page: Page) {
     return { email, isModifiable, isDeletable }
   }))
 
-  console.log('a')
-  // We make the assumption that only 2 channels exist, this is allways true (for now) in CI/CD
-  //   const channelRows = (await userTable.all()).slice(0, 2)
-  //   const failingChannels = await Promise.all(channelRows
-  //     .map(async (el) => {
-  //       const name = await el.locator('th:nth-child(1)').innerHTML()
-  //       const failing = await el.locator('td:nth-child(4)').innerHTML()
+  return members
+}
 
-//       return {
-//         name,
-//         failing,
-//       }
-//     }),
-//   )
+async function getOrgName(page: Page): Promise<string> {
+  await expect(page.locator('div.mr-2 > div:nth-child(1)')).toBeVisible()
+  const name = await page.locator('#organization-picker').innerText()
+
+  return name
 }
