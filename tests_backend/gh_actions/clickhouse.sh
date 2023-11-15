467830a52111dea3ceee961dda7ba534a3383cbb
WcaleNieWolny <isupermichael007@gmail.com>
2023-11-15 20:27:56 +0100

    Fix clickhouse.sql

--- tests_backend/gh_actions/clickhouse.sh
+++ tests_backend/gh_actions/clickhouse.sh
@@ -3,4 +3,5 @@ docker run -d \
     -v $(realpath ./ch_logs):/var/log/clickhouse-server/ \
     -p 18123:8123 \
     -p 19000:9000 \
+    -p 9440:9440 \
     --name some-clickhouse-server --ulimit nofile=262144:262144 clickhouse/clickhouse-server
