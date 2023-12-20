1c5099f8f6097de5606cf716faec4642e9219f39
WcaleNieWolny <isupermichael007@gmail.com>
2023-12-20 06:36:18 +0100

    Add missing d1 replication

--- supabase/migrations/20231204041758_d1_replication.sql
+++ supabase/migrations/20231204041758_d1_replication.sql
@@ -7,7 +7,7 @@ DECLARE
   request_id text;
 BEGIN 
   SELECT INTO request_id net.http_post(
-    url := 'http://host.docker.internal:8787',
+    url := 'http://host.docker.internal:6655',
     headers := jsonb_build_object(
       'Content-Type',
       'application/json'
@@ -20,6 +20,11 @@ BEGIN
    );
 END;
 $BODY$;
+
+-- with i as (
+--   (select * from json_each_text((row_to_json((select ROW(apps.*) from apps limit 1)))))
+-- )
+-- select (select json_agg((CASE WHEN i.value != '' THEN (i.value) ELSE 'null' END)) from i)
 
 CREATE OR REPLACE FUNCTION "public"."replicate_insert"() RETURNS trigger
    LANGUAGE plpgsql AS $$
@@ -46,7 +51,7 @@ BEGIN
     with i as (
         (select * from json_each_text((row_to_json(NEW))))
     )
-    (select format('UPDATE %s SET %s WHERE %s=%s', TG_ARGV[0], (select string_agg(format('"%s"=%s', i.key, (SELECT (CASE WHEN i.value != '' THEN (select format('''%s''', i.value)) ELSE 'NULL' END))), ', ') from i), TG_ARGV[1], (select i.value from i where i.key=TG_ARGV[1] limit 1))) INTO sql_query;
+    (select format('UPDATE %s SET %s WHERE %s="%s"', TG_ARGV[0], (select string_agg(format('"%s"=%s', i.key, (SELECT (CASE WHEN i.value != '' THEN (select format('''%s''', i.value)) ELSE 'NULL' END))), ', ') from i), TG_ARGV[1], (select i.value from i where i.key=TG_ARGV[1] limit 1))) INTO sql_query;
 
 
   PERFORM post_replication_sql(sql_query);
@@ -60,11 +65,43 @@ DECLARE
   request_id text;
   filter_value text;
 BEGIN
-  EXECUTE format('SELECT ($1."%s")::text', 'id') using OLD into filter_value;
-  SELECT format ('DELETE FROM %s WHERE %s=%s', TG_ARGV[0], TG_ARGV[1], filter_value) INTO sql_query;
+  EXECUTE format('SELECT ($1."%s")::text', TG_ARGV[1]) using OLD into filter_value;
+  SELECT format ('DELETE FROM %s WHERE %s="%s"', TG_ARGV[0], TG_ARGV[1], filter_value) INTO sql_query;
 
   PERFORM post_replication_sql(sql_query);
   RETURN OLD;
+END;$$;
+
+CREATE OR REPLACE FUNCTION "public"."replicate_drop_double"() RETURNS trigger
+   LANGUAGE plpgsql AS $$
+DECLARE
+  sql_query character varying;
+  request_id text;
+  filter_value text;
+  filter_value_two text;
+BEGIN
+  EXECUTE format('SELECT ($1."%s")::text', TG_ARGV[1]) using OLD into filter_value;
+  EXECUTE format('SELECT ($1."%s")::text', TG_ARGV[2]) using OLD into filter_value_two;
+  SELECT format ('DELETE FROM %s WHERE %s="%s" AND %s="%s"', TG_ARGV[0], TG_ARGV[1], filter_value, AG_ARGV[2], filter_value_two) INTO sql_query;
+
+  PERFORM post_replication_sql(sql_query);
+  RETURN OLD;
+END;$$;
+
+CREATE OR REPLACE FUNCTION "public"."replicate_update_double"() RETURNS trigger
+   LANGUAGE plpgsql AS $$
+DECLARE
+  sql_query character varying;
+  request_id text;
+BEGIN
+    with i as (
+        (select * from json_each_text((row_to_json(NEW))))
+    )
+    (select format('UPDATE %s SET %s WHERE %s="%s" AND %s="%s"', TG_ARGV[0], (select string_agg(format('"%s"=%s', i.key, (SELECT (CASE WHEN i.value != '' THEN (select format('''%s''', i.value)) ELSE 'NULL' END))), ', ') from i), TG_ARGV[1], (select i.value from i where i.key=TG_ARGV[1] limit 1), TG_ARGV[2], (select i.value from i where i.key=TG_ARGV[2] limit 1))) INTO sql_query;
+
+
+  PERFORM post_replication_sql(sql_query);
+  RETURN NEW;
 END;$$;
 
 -- EXECUTE format('SELECT ($1."%s" is distinct from $2."%s")', val.key, val.key) using NEW, OLD
@@ -93,7 +130,49 @@ CREATE TRIGGER replicate_version_drop
    BEFORE DELETE ON "public"."app_versions" FOR EACH ROW
    EXECUTE PROCEDURE "public"."replicate_drop"('app_versions', 'id');
 
+--
+CREATE TRIGGER replicate_apps_insert
+   BEFORE INSERT ON "public"."apps" FOR EACH ROW
+   EXECUTE PROCEDURE "public"."replicate_insert"('apps');
 
+CREATE TRIGGER replicate_apps_update
+   BEFORE UPDATE ON "public"."apps" FOR EACH ROW
+   EXECUTE PROCEDURE "public"."replicate_update"('apps', 'app_id');
+
+CREATE TRIGGER replicate_apps_drop
+   BEFORE DELETE ON "public"."apps" FOR EACH ROW
+   EXECUTE PROCEDURE "public"."replicate_drop"('apps', 'app_id');
+
+--
+CREATE TRIGGER replicate_channel_devices_insert
+   BEFORE INSERT ON "public"."channel_devices" FOR EACH ROW
+   EXECUTE PROCEDURE "public"."replicate_insert"('channel_devices');
+
+CREATE TRIGGER replicate_channel_devices_update
+   BEFORE UPDATE ON "public"."channel_devices" FOR EACH ROW
+   EXECUTE PROCEDURE "public"."replicate_update_double"('channel_devices', 'device_id', 'app_id');
+
+CREATE TRIGGER replicate_channel_devices_drop
+   BEFORE DELETE ON "public"."channel_devices" FOR EACH ROW
+   EXECUTE PROCEDURE "public"."replicate_drop_double"('channel_devices', 'device_id', 'app_id');
+
+--
+CREATE TRIGGER replicate_devices_override_insert
+   BEFORE INSERT ON "public"."devices_override" FOR EACH ROW
+   EXECUTE PROCEDURE "public"."replicate_insert"('devices_override');
+
+CREATE TRIGGER replicate_devices_override_update
+   BEFORE UPDATE ON "public"."devices_override" FOR EACH ROW
+   EXECUTE PROCEDURE "public"."replicate_update_double"('devices_override', 'device_id', 'app_id');
+
+CREATE TRIGGER replicate_devices_override_drop
+   BEFORE DELETE ON "public"."devices_override" FOR EACH ROW
+   EXECUTE PROCEDURE "public"."replicate_drop_double"('devices_override', 'device_id', 'app_id');
+
+-- with i as (
+--   (select * from json_each_text((row_to_json((select ROW(apps.*) from apps limit 1)))))
+-- )
+-- select (select json_agg(i.value) from i)
 -- INSERT INTO apps (id, created_at, name, app_id, version, created_by, updated_at, public, disableAutoUpdateUnderNative, enableAbTesting, enable_progressive_deploy, secondaryVersionPercentage, beta, ios, android, allow_device_self_set, allow_emulator, allow_dev, disableAutoUpdate) VALUES('22', '2023-12-04T04:31:09.255645+00:00', 'productionn', 'com.demo.app', '9654', '6aa76066-55ef-4238-ade6-0b32334a4097', '2023-12-04T06:41:27.420552+00:00', 'true', 'true', 'false', 'false', '0', 'false', 'false', 'true', 'true', 'true', 'true', 'major')
 -- CREATE TRIGGER replicate_c
 --    BEFORE UPDATE ON "public"."channels" FOR EACH ROW
