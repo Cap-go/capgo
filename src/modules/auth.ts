7bf95ce98ef69a24b0156d7dd8b25140cfd69f4c
WcaleNieWolny <isupermichael007@gmail.com>
2023-12-30 12:20:04 +0100

    Clientside prevent 2FA cheat

--- src/modules/auth.ts
+++ src/modules/auth.ts
@@ -21,9 +21,9 @@ async function guard(next: any, to: string, from: string) {
     return
   }
 
-  // if (mfaData.currentLevel === 'aal1' && mfaData.nextLevel === 'aal2') {
-  //   return next('/login')
-  // }
+  if (mfaData.currentLevel === 'aal1' && mfaData.nextLevel === 'aal2') {
+    return next('/login')
+  }
 
   if (auth.user && !main.auth) {
     main.auth = auth.user
