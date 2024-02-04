ed748afac137fb0d36270f028cf40db3ea72880c
WcaleNieWolny <isupermichael007@gmail.com>
2024-02-04 06:11:59 +0100

    Move migration to a seperate file

--- supabase/migrations/20240202175107_fix_cli_upload.sql
+++ supabase/migrations/20240202175107_fix_cli_upload.sql
@@ -22,24 +22,3 @@ USING (
   AND 
   "public"."is_allowed_action"((("current_setting"('request.headers'::"text", true))::"json" ->> 'capgkey'::"text"), "app_id")
 );
-
-DROP POLICY "Allow apikey to insert" ON "public"."app_versions";
-DROP POLICY "Allow org member's apikey to insert" ON "public"."app_versions";
-CREATE POLICY "Allow apikey to insert" ON "public"."app_versions" FOR INSERT TO "anon"
-WITH CHECK (
-  (
-    ("public"."is_allowed_capgkey"((("current_setting"('request.headers'::"text", true))::"json" ->> 'capgkey'::"text"), '{upload,write,all}'::"key_mode"[], "app_id") AND "public"."is_allowed_action"((("current_setting"('request.headers'::text, true))::"json" ->> 'capgkey'::"text")))
-  ) OR (
-    "public"."is_allowed_capgkey"((("current_setting"('request.headers'::"text", true))::"json" ->> 'capgkey'::text), '{upload,write,all}'::"key_mode"[], "app_id", NULL::bigint, 'upload'::"user_min_right", "user_id") AND "public"."is_allowed_action"((("current_setting"('request.headers'::text, true))::json ->> 'capgkey'::text), "app_id")
-  )
-);
-
-DROP POLICY "Allow apikey to select" ON "public"."app_versions";
--- DROP POLICY "Allow org members to select" on "public"."app_versions";
-CREATE POLICY "Allow apikey to select" ON "public"."app_versions" FOR SELECT TO "anon" USING (
-  (
-    "public"."is_allowed_capgkey"((("current_setting"('request.headers'::"text", true))::json ->> 'capgkey'::"text"), '{read,all,upload,write}'::"key_mode"[], "app_id") OR 
-    "public"."is_allowed_capgkey"((("current_setting"('request.headers'::"text", true))::json ->> 'capgkey'::"text"), '{read,all,upload,write}'::"key_mode"[], "app_id", 'read'::"user_min_right", "user_id")
-  )
-);
--- ((is_allowed_capgkey(((current_setting('request.headers'::text, true))::json ->> 'capgkey'::text), '{upload,write,all}'::key_mode[], app_id) AND is_allowed_action(((current_setting('request.headers'::text, true))::json ->> 'capgkey'::text))) OR (is_allowed_capgkey(((current_setting('request.headers'::text, true))::json ->> 'capgkey'::text), '{upload,write,all}'::key_mode[], app_id, NULL::bigint, 'upload'::user_min_right, user_id) AND is_allowed_action(((current_setting('request.headers'::text, true))::json ->> 'capgkey'::text), app_id)))
