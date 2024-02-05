13962aaea50f5cc6a2941debac58de356570ece1
Martin Donadieu <martindonadieu@gmail.com>
2024-02-05 14:11:24 +0000

    fix: use better routing

--- supabase/functions/updates_debug/index.ts
+++ supabase/functions/updates_debug/index.ts
@@ -1,5 +1,9 @@
 import { app } from '../_backend/private/plugins/updates.ts'
+import { Hono } from 'npm:hono'
 
 const functionName = 'updates_debug'
+const appGlobal = new Hono().basePath(`/${functionName}`)
 
-Deno.serve(app.basePath(`/${functionName}`).fetch)
+appGlobal.route('/', app)
+
+Deno.serve(appGlobal.fetch)
