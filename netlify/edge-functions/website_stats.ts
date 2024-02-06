e4e9f4d383de8ea9bb12351423f6dcc081904642
Martin Donadieu <martindonadieu@gmail.com>
2024-02-06 04:45:41 +0000

    fix: netlify deploy

--- netlify/edge-functions/website_stats.ts
+++ netlify/edge-functions/website_stats.ts
@@ -1,4 +1,4 @@
 import { handle } from 'https://deno.land/x/hono@v4.0.0-rc.3/adapter/netlify/mod.ts'
-import { app } from '../supabase/functions/_backend/private/plugins/channel_self.ts'
+import { app } from '../../supabase/functions/_backend/private/webapps/public_stats.ts'
 
 export default handle(app as any)
