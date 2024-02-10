b159476e3b497fe7c916fea2c8b49bc817fb06c1
WcaleNieWolny <isupermichael007@gmail.com>
2024-02-10 15:46:08 +0100

    Fix stats graph height

--- src/components/dashboard/UsageCard.vue
+++ src/components/dashboard/UsageCard.vue
@@ -47,7 +47,7 @@ const lastDayEvolution = computed(() => {
 </script>
 
 <template>
-  <div class="flex flex-col bg-white border rounded-lg shadow-lg col-span-full border-slate-200 sm:col-span-6 xl:col-span-4 dark:border-slate-900 dark:bg-gray-800" :class="{ 'h-[460px]': props.datas?.length }">
+  <div class="flex flex-col bg-white border rounded-lg shadow-lg col-span-full border-slate-200 sm:col-span-6 xl:col-span-4 dark:border-slate-900 dark:bg-gray-800 h-[460px]">
     <div class="px-5 pt-3">
       <div class="flex flex-row">
         <h2 class="mb-2 mr-4 text-2xl font-semibold text-slate-800 dark:text-white">
