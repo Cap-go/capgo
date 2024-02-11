7ba1f5a3465ae43f7e83e6d21507775061b7c725
WcaleNieWolny <isupermichael007@gmail.com>
2024-02-11 06:53:33 +0100

    chore: fix case in migration

--- supabase/migrations/20240211054205_drop_unused_jobqueue_triggers.sql
+++ supabase/migrations/20240211054205_drop_unused_jobqueue_triggers.sql
@@ -2,6 +2,6 @@ drop trigger on_app_delete_sql on apps;
 drop trigger on_app_versions_delete_sql on app_versions;
 drop trigger on_device_delete_sql on devices;
 
-DROP FUNCTION on_app_version_delete_sql();
-DROP FUNCTION on_app_delete_sql();
-DROP FUNCTION on_device_delete_sql();
+drop function on_app_version_delete_sql();
+drop function on_app_delete_sql();
+drop function on_device_delete_sql();
