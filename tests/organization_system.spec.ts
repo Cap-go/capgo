0128a410b98f007b976df9bfd358f6d347ba2a76
WcaleNieWolny <isupermichael007@gmail.com>
2024-02-12 13:05:34 +0100

    Replace crypto.randomUUID with math.random

--- tests/organization_system.spec.ts
+++ tests/organization_system.spec.ts
@@ -659,7 +659,22 @@ test.describe('Test organization system permissions', () => {
         await page.click('#inforow-input')
 
         if (permission.setDeviceCustomId) {
-          await page.fill('#inforow-input', `test-${crypto.randomUUID()}`)
+          function createUUID() {
+            // http://www.ietf.org/rfc/rfc4122.txt
+            const s = [] as string[]
+            const hexDigits: string = '0123456789abcdef'
+            for (let i = 0; i < 36; i++)
+              s[i] = hexDigits.substr(Math.floor(Math.random() * 0x10), 1)
+
+            s[14] = '4' // bits 12-15 of the time_hi_and_version field to 0010
+            s[19] = hexDigits.substr((s[19] as any & 0x3) | 0x8, 1) // bits 6-7 of the clock_seq_hi_and_reserved to 01
+            s[8] = s[13] = s[18] = s[23] = '-'
+
+            const uuid = s.join('')
+            return uuid
+          }
+
+          await page.fill('#inforow-input', `test-${createUUID()}`)
           await expectPopout(page, 'Custom ID saved')
         }
         else {
