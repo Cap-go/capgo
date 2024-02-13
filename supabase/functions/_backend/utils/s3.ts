8ba63232d8d30a1a7c4461ca62609b6d7b5ee138
Martin Donadieu <martindonadieu@gmail.com>
2024-02-13 03:09:35 +0000

    fix: build cloudflare

--- supabase/functions/_backend/utils/s3.ts
+++ supabase/functions/_backend/utils/s3.ts
@@ -1,5 +1,6 @@
 import type { Context } from 'hono'
-import { Client } from 'minio'
+import { S3Client } from '@capgo/s3-lite-client'
+
 
 import { getEnv } from './utils.ts'
 
@@ -8,6 +9,7 @@ function initS3(c: Context, clientSideOnly?: boolean) {
   const access_key_secret = getEnv(c, 'S3_SECRET_ACCESS_KEY')
   const storageEndpoint = !clientSideOnly ? getEnv(c, 'S3_ENDPOINT') : getEnv(c, 'S3_ENDPOINT').replace('host.docker.internal', '0.0.0.0')
   const storageRegion = getEnv(c, 'S3_REGION')
+  const bucket = getEnv(c, 'S3_BUCKET')
   const storagePort = Number.parseInt(getEnv(c, 'S3_PORT'))
   const storageUseSsl = getEnv(c, 'S3_SSL').toLocaleLowerCase() === 'true'
   const params = {
@@ -17,46 +19,39 @@ function initS3(c: Context, clientSideOnly?: boolean) {
     port: storagePort && !Number.isNaN(storagePort) ? storagePort : undefined,
     accessKey: access_key_id,
     secretKey: access_key_secret,
+    bucket,
   }
   console.log('initS3', params)
-  return new Client(params)
+  return new S3Client(params)
 }
 
 async function getUploadUrl(c: Context, fileId: string, expirySeconds = 60) {
   const client = initS3(c, true)
 
-  const bucket = getEnv(c, 'S3_BUCKET')
-  return client.presignedPutObject(bucket, fileId, expirySeconds)
+  return client.getPresignedUrl('PUT', fileId, { expirySeconds })
 }
 
 function deleteObject(c: Context, fileId: string) {
   const client = initS3(c)
   const bucket = getEnv(c, 'S3_BUCKET')
-  return client.removeObject(bucket, fileId)
+  return client.deleteObject(fileId)
 }
 
 function checkIfExist(c: Context, fileId: string) {
   const client = initS3(c)
-  const bucket = getEnv(c, 'S3_BUCKET')
-  return new Promise((resolve) => {
-    client.getPartialObject(bucket, fileId, 0, 1, (err) => {
-      resolve(!err)
-    })
-  })
+  return client.exists(fileId)
 }
 
 async function getSignedUrl(c: Context, fileId: string, expirySeconds: number) {
   const client = initS3(c, true)
 
-  const bucket = getEnv(c, 'S3_BUCKET')
-  return client.presignedGetObject(bucket, fileId, expirySeconds)
+  return client.getPresignedUrl('GET', fileId, { expirySeconds })
 }
 
 async function getSizeChecksum(c: Context, fileId: string) {
   const client = initS3(c)
-  const bucket = getEnv(c, 'S3_BUCKET')
-  const { size, metaData } = await client.statObject(bucket, fileId)
-  const checksum = metaData['x-amz-meta-crc32']
+  const { size, metadata } = await client.statObject(fileId)
+  const checksum = metadata['x-amz-meta-crc32']
   return { size, checksum }
 }
 
