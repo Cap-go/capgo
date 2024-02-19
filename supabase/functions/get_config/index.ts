5e743c55b25b4781ee0bc549654311421f790097
Martin Donadieu <martindonadieu@gmail.com>
2024-02-19 01:04:43 +0100

    fix: lint issue

--- supabase/functions/get_config/index.ts
+++ supabase/functions/get_config/index.ts
@@ -1,6 +1,7 @@
 import { Hono } from 'hono/tiny'
 import { app } from '../_backend/private/webapps/config.ts'
-// TODO: deprecated remove when everyone use the new CLI 
+
+// TODO: deprecated remove when everyone use the new CLI
 const functionName = 'get_config'
 const appGlobal = new Hono().basePath(`/${functionName}`)
 
