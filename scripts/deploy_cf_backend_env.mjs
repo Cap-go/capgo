6b093142d23f4a34636956a14c925255e243c883
Martin Donadieu <martindonadieu@gmail.com>
2024-02-23 00:33:05 +0100

    fix: use new script for deploy

--- scripts/deploy_cf_backend_env.mjs
+++ scripts/deploy_cf_backend_env.mjs
@@ -5,14 +5,18 @@ import { exit } from 'process';
 
 // Check if the environment file name is provided as a command-line argument
 const envFileName = process.argv[2];
+const envName = process.argv[3];
 if (!envFileName) {
-  console.error('Please provide the environment file name as a parameter.');
+  console.error('Please provide the environment file name as second parameter.');
+  exit(1);
+}
+if (!envName) {
+  console.error('Please provide the worker name as third parameter.');
   exit(1);
 }
 
 // Read the environment file
 const envFilePath = resolve(envFileName);
-const nameWorker = envFileName === '.env' ? 'capgo_api' : 'capgo_api-alpha';
 let env;
 try {
   env = readFileSync(envFilePath, 'utf8');
@@ -23,6 +27,8 @@ try {
 
 const envVars = env.split('\n').filter((line) => line.trim() !== '').filter((line) => !line.startsWith('#'));
 
+console.log('Environment file', envFileName);
+console.log('worker Name', envName);
 console.log('Environment variables', envVars);
 
 const envJson = envVars.reduce((acc, envVar) => {
@@ -36,7 +42,7 @@ const secrets = JSON.stringify(envJson, null, 2);
 console.log('Secrets', secrets);
 
 // Construct the command to execute
-const command = `echo '${secrets.replace(/'/g, "'\\''")}' | wrangler secret:bulk --name ${nameWorker}`;
+const command = `echo '${secrets.replace(/'/g, "'\\''")}' | wrangler secret:bulk --name ${envName}`;
 
 try {
   // Execute the command
