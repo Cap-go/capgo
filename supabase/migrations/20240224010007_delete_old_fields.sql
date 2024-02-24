7184ccbc9ac50f456d8d0b7c0723ef760adf7cac
Martin Donadieu <martindonadieu@gmail.com>
2024-02-24 02:45:13 +0100

    fix: remove migration, the field where only in prod

--- supabase/migrations/20240224010007_delete_old_fields.sql
+++
@@ -1,8 +0,0 @@
--- remove channel filde from table plans
-ALTER TABLE 'app' DROP COLUMN channel;
-ALTER TABLE 'channel' DROP COLUMN channel;
-ALTER TABLE 'update' DROP COLUMN channel;
-ALTER TABLE 'version' DROP COLUMN channel;
-ALTER TABLE 'shared' DROP COLUMN channel;
-ALTER TABLE 'abtest' DROP COLUMN channel;
-ALTER TABLE 'progressive_deploy' DROP COLUMN channel;
