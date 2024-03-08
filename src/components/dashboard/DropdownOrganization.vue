de7fb055e8638294e729a4db8e329a3ac0a87ffe
Martin Donadieu <martindonadieu@gmail.com>
2024-03-08 04:07:05 +0000

    fix: v-if issue in channel

--- src/components/dashboard/DropdownOrganization.vue
+++ src/components/dashboard/DropdownOrganization.vue
@@ -121,9 +121,9 @@ watchEffect(async () => {
 </script>
 
 <template>
-  <div v-if="show">
+  <div v-show="show">
     <button
-      v-if="currentOrganization" id="organization-picker" data-dropdown-toggle="dropdown-org"
+      v-show="currentOrganization" id="organization-picker" data-dropdown-toggle="dropdown-org"
       class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
       type="button"
     >
