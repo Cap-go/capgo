7a3e0ef47e1a5ee30fc2ae13eb6eefe398de4a61
WcaleNieWolny <50914789+WcaleNieWolny@users.noreply.github.com>
2024-04-17 12:41:13 +0000

    fix: Uncomment cron fix

--- supabase/migrations/20240405132112_better_cron.sql
+++ supabase/migrations/20240405132112_better_cron.sql
@@ -1,11 +1,10 @@
 -- remove hack to run subminute cron jobs
 
--- Commented out due to local cron plugin being old compared to the production one
--- SELECT cron.unschedule('process_tasks_subminute' );
--- -- call process_current_jobs_if_unlocked every 20 seconds
--- SELECT cron.schedule(
---     'process_current_jobs_if_unlocked',
---     '20 seconds',
---     $$SELECT process_current_jobs_if_unlocked();$$
--- );
--- drop function "public"."schedule_jobs"();
+SELECT cron.unschedule('process_tasks_subminute' );
+-- call process_current_jobs_if_unlocked every 20 seconds
+SELECT cron.schedule(
+    'process_current_jobs_if_unlocked',
+    '20 seconds',
+    $$SELECT process_current_jobs_if_unlocked();$$
+);
+drop function "public"."schedule_jobs"();
