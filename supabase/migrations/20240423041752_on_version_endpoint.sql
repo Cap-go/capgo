b77acbce1f0fed07dc70d2213def0be731d8f5d6
Martin Donadieu <martindonadieu@gmail.com>
2024-04-23 16:48:24 +0100

    fix: drop trigger issue

--- supabase/migrations/20240423041752_on_version_endpoint.sql
+++ supabase/migrations/20240423041752_on_version_endpoint.sql
@@ -4,7 +4,7 @@ AFTER INSERT ON public.app_versions
 FOR EACH ROW 
 EXECUTE FUNCTION public.trigger_http_queue_post_to_function('on_version_create', 'cloudflare');
 
-DROP TRIGGER on_version_delete ON "public"."app_versions";
+DROP TRIGGER on_version_update ON "public"."app_versions";
 CREATE TRIGGER on_version_update 
 AFTER UPDATE ON public.app_versions 
 FOR EACH ROW 
