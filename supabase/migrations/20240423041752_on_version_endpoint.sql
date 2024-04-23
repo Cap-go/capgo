3810b730f92b0cb762d41753fed4c69e028f0040
Martin Donadieu <martindonadieu@gmail.com>
2024-04-23 07:11:52 +0100

    fix: switch to new endpoint

---
+++ supabase/migrations/20240423041752_on_version_endpoint.sql
@@ -0,0 +1,11 @@
+DROP TRIGGER on_version_create ON "public"."app_versions";
+CREATE TRIGGER on_version_create 
+AFTER INSERT ON public.app_versions 
+FOR EACH ROW 
+EXECUTE FUNCTION public.trigger_http_queue_post_to_function('on_version_create', 'cloudflare');
+
+DROP TRIGGER on_version_delete ON "public"."app_versions";
+CREATE TRIGGER on_version_update 
+AFTER UPDATE ON public.app_versions 
+FOR EACH ROW 
+EXECUTE FUNCTION public.trigger_http_queue_post_to_function('on_version_update', 'cloudflare');
---
+++ supabase/migrations/20240423061038_http_post_helper_cloudflare.sql
@@ -0,0 +1,37 @@
+CREATE OR REPLACE FUNCTION public.http_post_helper(function_name text, function_type text, body jsonb) 
+RETURNS bigint 
+LANGUAGE plpgsql 
+SECURITY DEFINER
+AS $BODY$
+<<declared>>
+DECLARE 
+  request_id text;
+  headers jsonb;
+  url text;
+BEGIN 
+  headers := jsonb_build_object(
+    'Content-Type',
+    'application/json',
+    'apisecret',
+    get_apikey()
+  );
+  -- Determine the URL based on the function_type
+  CASE function_type
+  WHEN 'netlify' THEN
+    url := get_netlify_function_url() || '/triggers/' || function_name;
+  WHEN 'cloudflare' THEN
+    url := get_cloudflare_function_url() || '/triggers/' || function_name;
+  ELSE
+    url := get_db_url() || '/functions/v1/triggers/' || function_name;
+  END CASE;
+
+  -- Make an async HTTP POST request using pg_net
+  SELECT INTO request_id net.http_post(
+    url := declared.url,
+    headers := declared.headers,
+    body := body,
+    timeout_milliseconds := 15000
+  );
+  return request_id;
+END;
+$BODY$;
