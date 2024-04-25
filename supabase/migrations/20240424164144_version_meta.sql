f41c674b7240c437c7847c84eaa2b79b9c594fc0
Martin Donadieu <martindonadieu@gmail.com>
2024-04-25 11:18:23 +0100

    fix: revert on_version_create and on_version_update

--- supabase/migrations/20240424164144_version_meta.sql
+++ supabase/migrations/20240424164144_version_meta.sql
@@ -38,3 +38,15 @@ $$ LANGUAGE plpgsql;
 
 -- Schedule the function to run daily at 1:00 AM
 SELECT cron.schedule('Update Daily Storage', '0 1 * * *', $$SELECT update_daily_storage()$$);
+
+DROP TRIGGER on_version_create ON "public"."app_versions";
+CREATE TRIGGER on_version_create 
+AFTER INSERT ON public.app_versions 
+FOR EACH ROW 
+EXECUTE FUNCTION public.trigger_http_queue_post_to_function('on_version_create');
+
+DROP TRIGGER on_version_update ON "public"."app_versions";
+CREATE TRIGGER on_version_update 
+AFTER UPDATE ON public.app_versions 
+FOR EACH ROW 
+EXECUTE FUNCTION public.trigger_http_queue_post_to_function('on_version_update');
