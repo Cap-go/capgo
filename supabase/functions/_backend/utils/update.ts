81b83e6f1d6c2885416e75eef92670c958f974e0
Martin Donadieu <martindonadieu@gmail.com>
2024-05-22 03:57:43 +0100

    fix: add timeout to force deco

--- supabase/functions/_backend/utils/update.ts
+++ supabase/functions/_backend/utils/update.ts
@@ -38,14 +38,15 @@ function resToVersion(plugin_version: string, signedURL: string, version: Databa
 function getDrizzlePostgres(c: Context) {
   if (c.env.HYPERDRIVE) {
     console.log('HYPERDRIVE', c.env.HYPERDRIVE.connectionString)
-    return postgres(c.env.HYPERDRIVE.connectionString, { prepare: false })
+    return postgres(c.env.HYPERDRIVE.connectionString, { prepare: false, timeout: 2 })
   }
-  else if (getEnv(c, 'CUSTOM_SUPABASE_DB_URL')) {
-    console.log('CUSTOM_SUPABASE_DB_URL', getEnv(c, 'CUSTOM_SUPABASE_DB_URL'))
-    return postgres(getEnv(c, 'CUSTOM_SUPABASE_DB_URL'))
-  }
+  else
+    if (getEnv(c, 'CUSTOM_SUPABASE_DB_URL')) {
+      console.log('CUSTOM_SUPABASE_DB_URL', getEnv(c, 'CUSTOM_SUPABASE_DB_URL'))
+      return postgres(getEnv(c, 'CUSTOM_SUPABASE_DB_URL'), { timeout: 2 })
+    }
   console.log('SUPABASE_DB_URL', getEnv(c, 'SUPABASE_DB_URL'))
-  return postgres(getEnv(c, 'SUPABASE_DB_URL'))
+  return postgres(getEnv(c, 'SUPABASE_DB_URL'), { timeout: 2 })
 }
 
 async function requestInfosPostgres(
