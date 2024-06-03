15c259171eb8191f1202096ca5c9a28582270abb
Martin Donadieu <martindonadieu@gmail.com>
2024-06-04 01:34:47 +0200

    fix: get_cycle_info_org function

--- supabase/migrations/20240513055310_base.sql
+++ supabase/migrations/20240513055310_base.sql
@@ -627,13 +627,15 @@ $$;
 
 ALTER FUNCTION "public"."get_customer_counts"() OWNER TO "postgres";
 
-CREATE OR REPLACE FUNCTION "public"."get_cycle_info_org"("orgid" "uuid") RETURNS TABLE("subscription_anchor_start" timestamp with time zone, "subscription_anchor_end" timestamp with time zone)
-    LANGUAGE "plpgsql"
-    AS $$
+CREATE OR REPLACE FUNCTION "public"."get_cycle_info_org"("orgid" "uuid")
+RETURNS TABLE (
+    subscription_anchor_start timestamp with time zone,
+    subscription_anchor_end timestamp with time zone
+) AS $$
 DECLARE
     customer_id_var text;
     stripe_info_row stripe_info%ROWTYPE;
-    anchor_day int;
+    anchor_day INTERVAL;
     start_date timestamp with time zone;
     end_date timestamp with time zone;
 BEGIN
@@ -642,14 +644,14 @@ BEGIN
     -- Get the stripe_info using the customer_id
     SELECT * INTO stripe_info_row FROM stripe_info WHERE customer_id = customer_id_var;
 
-    -- Extract the day of the month from subscription_anchor_start, default to 1 if null
-    anchor_day := COALESCE(EXTRACT(DAY FROM stripe_info_row.subscription_anchor_start), 1);
+    -- Extract the day of the month from subscription_anchor_start as an INTERVAL, default to '0 DAYS' if null
+    anchor_day := COALESCE(stripe_info_row.subscription_anchor_start - date_trunc('MONTH', stripe_info_row.subscription_anchor_start), '0 DAYS'::INTERVAL);
 
     -- Determine the start date based on the anchor day and current date
-    IF anchor_day > EXTRACT(DAY FROM now()) THEN
-        start_date := date_trunc('MONTH', now() - INTERVAL '1 MONTH') + (anchor_day - 1) * INTERVAL '0 DAY';
+    IF anchor_day > now() - date_trunc('MONTH', now()) THEN
+        start_date := date_trunc('MONTH', now() - INTERVAL '1 MONTH') + anchor_day;
     ELSE
-        start_date := date_trunc('MONTH', now()) + (anchor_day - 1) * INTERVAL '1 DAY';
+        start_date := date_trunc('MONTH', now()) + anchor_day;
     END IF;
 
     -- Calculate the end date
@@ -658,7 +660,7 @@ BEGIN
     RETURN QUERY
     SELECT start_date, end_date;
 END;
-$$;
+$$ LANGUAGE plpgsql;
 
 ALTER FUNCTION "public"."get_cycle_info_org"("orgid" "uuid") OWNER TO "postgres";
 
