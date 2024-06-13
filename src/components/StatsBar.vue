9281e9e139562f59166d832925de0632247828dc
Martin Donadieu <martindonadieu@gmail.com>
2024-06-13 20:13:09 +0100

    fix: disable effect then no hover text

--- src/components/StatsBar.vue
+++ src/components/StatsBar.vue
@@ -9,15 +9,24 @@ const refStats = toRef(() => props.stats)
 <template>
   <template v-for="s, i in refStats" :key="i">
     <a
-      :href="s.link || '#'" class="flex flex-col items-center w-full group"
-      :class="{ 'hover:bg-gray-100 dark:hover:bg-gray-800': s.link, 'p-10 sm:px-12 lg:px-16 lg:py-14': !props.mini, 'p-5 sm:px-7 lg:px-6 lg:py-4': props.mini }"
+      :href="s.link || '#'" class="flex flex-col items-center w-full"
+      :class="{
+        'group hover:bg-gray-100 dark:hover:bg-gray-800': s.link && (s.hoverLabel || s.link),
+        'p-10 sm:px-12 lg:px-16 lg:py-14': !props.mini,
+        'p-5 sm:px-7 lg:px-6 lg:py-4': props.mini,
+      }"
     >
-      <span class="text-center duration-100 ease-in scale-100" :class="{ 'group-hover:scale-125': s.link }">
+      <span class="text-center duration-100 ease-in scale-100" :class="{ 'group-hover:scale-125': s.link && (s.hoverLabel || s.link) }">
         <p
           v-if="!!s.value"
           id="stats-val"
-          class="font-bold dark:text-white font-pj lg:order-1 lg:mt-3 group-hover:hidden"
-          :class="{ 'text-5xl': !props.mini, 'text-3xl': props.mini }"
+          class="font-bold dark:text-white font-pj lg:order-1 lg:mt-3"
+          :class="{
+            'text-5xl group-hover:hidden': !props.mini && (s.link && (s.hoverLabel || s.link)),
+            'text-3xl group-hover:hidden': props.mini && (s.link && (s.hoverLabel || s.link)),
+            'text-5xl': !props.mini,
+            'text-3xl': props.mini,
+          }"
         >
           {{ s.value }}
         </p>
@@ -25,9 +34,9 @@ const refStats = toRef(() => props.stats)
           <Spinner size="w-10 h-10 ml-auto mr-auto" />
         </div>
         <div class="flex flex-row-reverse items-center justify-center flex-column">
-          <h3 class="mt-5 text-sm font-bold tracking-widest text-gray-400 uppercase transition-opacity duration-300 font-pj lg:order-2 lg:mt-0">
-            <span class="group-hover:hidden">{{ s.label }}</span>
-            <span class="hidden group-hover:inline">{{ s.hoverLabel || s.label }}</span>
+          <h3 class="mt-5 text-sm font-bold tracking-widest text-gray-400 uppercase font-pj lg:order-2 lg:mt-0">
+            <span :class="{ 'group-hover:hidden': s.link && (s.hoverLabel || s.link) }">{{ s.label }}</span>
+            <span v-if="s.link && (s.hoverLabel || s.link)" class="hidden group-hover:inline">{{ s.hoverLabel || s.label }}</span>
           </h3>
           <InformationInfo v-if="!!s.informationIcon" class="ml-1" @click="s.informationIcon" />
         </div>
