9b153e541c80c77c993dfddffac7a908f3e1e7ca
Martin Donadieu <martindonadieu@gmail.com>
2024-06-25 03:52:07 +0100

    fix: add minio debug

--- tests_cli/cliUtils.ts
+++ tests_cli/cliUtils.ts
@@ -23,7 +23,7 @@ function generateDefaultJsonCliConfig(baseUrl: URL) {
         localS3: true,
         localHost: 'http://localhost:5173',
         localWebHost: 'http://localhost:5173',
-        localSupa: 'http://127.0.0.1:54321',
+        localSupa: 'http://localhost:54321',
         localSupaAnon: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0',
       },
     },
