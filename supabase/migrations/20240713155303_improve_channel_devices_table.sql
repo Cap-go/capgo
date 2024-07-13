a642dd2351667c4aadf4ba839d4c81fc3b7ed975
Martin Donadieu <martindonadieu@gmail.com>
2024-07-13 19:40:36 +0300

    fix: migration

--- supabase/migrations/20240713155303_improve_channel_devices_table.sql
+++ supabase/migrations/20240713155303_improve_channel_devices_table.sql
@@ -1,5 +1,14 @@
 -- Migration file: improve_channel_devices_table.sql
 
+-- First, drop the existing primary key
+ALTER TABLE public.channel_devices 
+DROP CONSTRAINT channel_devices_pkey;
+
+-- Then, add the new primary key on the 'id' column
+ALTER TABLE public.channel_devices 
+ADD CONSTRAINT channel_devices_pkey PRIMARY KEY (id);
+
+
 -- Step 1: Remove the existing primary key constraint
 ALTER TABLE "public"."channel_devices" DROP CONSTRAINT IF EXISTS "channel_devices_pkey";
 
@@ -14,10 +23,3 @@ DROP INDEX IF EXISTS "idx_app_id_device_id_channel_devices";
 CREATE UNIQUE INDEX "idx_app_id_device_id_channel_id_channel_devices" 
 ON "public"."channel_devices" ("app_id", "device_id", "channel_id");
 
--- Step 5: Modify the id column to remove GENERATED ALWAYS and make it nullable
-ALTER TABLE "public"."channel_devices" 
-ALTER COLUMN "id" DROP DEFAULT,
-ALTER COLUMN "id" DROP NOT NULL;
-
--- Step 6: Drop the sequence if it exists
-DROP SEQUENCE IF EXISTS "public"."channel_devices_id_seq";
