98e0e270afd90d965c491ffa4712ba7d9bf3f1e6
WcaleNieWolny <michael@capgo.app>
2024-08-02 11:06:35 +0000

    🗃️ (feat): More sync between old and new

--- supabase/migrations/20240629035932_fix_fields_db_convention.sql
+++ supabase/migrations/20240629035932_fix_fields_db_convention.sql
@@ -34,10 +34,9 @@ $$;
 -- Add dummy columns
 
 ALTER TABLE channels 
-ADD COLUMN "secondaryVersionPercentage" double precision;
-
-ALTER TABLE channels 
 ADD COLUMN "secondVersion" bigint;
+
+--- 
 
 ALTER TABLE app_versions 
 ADD COLUMN "minUpdateVersion" character varying;
@@ -53,5 +52,50 @@ CREATE OR REPLACE FUNCTION "public"."sync_min_update_version"() RETURNS "trigger
   RETURN NEW;
 END;$$;
 
+UPDATE app_versions
+SET "minUpdateVersion"="min_update_version";
+
 CREATE OR REPLACE TRIGGER "sync_min_update_version" BEFORE UPDATE ON "public"."app_versions" FOR EACH ROW EXECUTE FUNCTION "public"."sync_min_update_version"();
+
+-- 
+
+ALTER TABLE channels 
+ADD COLUMN "secondaryVersionPercentage" double precision;
+
+-- UPDATE app_versions
+-- SET "secondaryVersionPercentage"="secondary_version_percentage";
+
+
+-- CREATE OR REPLACE FUNCTION "public"."sync_secondary_version_percentage"() RETURNS "trigger"
+--     LANGUAGE "plpgsql"
+--     AS $$BEGIN
+--   IF NEW."secondaryVersionPercentage" IS DISTINCT FROM OLD."secondaryVersionPercentage"
+--   THEN
+--     NEW.secondary_version_percentage = NEW."secondaryVersionPercentage";
+--   END IF;
+
+--   RETURN NEW;
+-- END;$$;
+
+-- CREATE OR REPLACE TRIGGER "sync_secondary_version_percentage" BEFORE UPDATE ON "public"."channels" FOR EACH ROW EXECUTE FUNCTION "public"."sync_secondary_version_percentage"();
+
+
+ALTER TABLE channels 
+ADD COLUMN "disableAutoUpdate" "public"."disable_update";
+
+UPDATE channels
+SET "disableAutoUpdate"="disable_auto_update";
+
+CREATE OR REPLACE FUNCTION "public"."sync_disable_auto_update"() RETURNS "trigger"
+    LANGUAGE "plpgsql"
+    AS $$BEGIN
+  IF NEW."disableAutoUpdate" IS DISTINCT FROM OLD."disableAutoUpdate"
+  THEN
+    NEW.disable_auto_update = NEW."disableAutoUpdate";
+  END IF;
+
+  RETURN NEW;
+END;$$;
+
+CREATE OR REPLACE TRIGGER "sync_disable_auto_update" BEFORE UPDATE ON "public"."channels" FOR EACH ROW EXECUTE FUNCTION "public"."sync_disable_auto_update"();
 
