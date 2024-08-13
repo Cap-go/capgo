82f32939645f2f9281d2819651c54f5253da31fe
Martin Donadieu <martindonadieu@gmail.com>
2024-08-13 04:27:56 +0100

    fix: sec issue

--- src/pages/dashboard/settings/organization/Plans.vue
+++ src/pages/dashboard/settings/organization/Plans.vue
@@ -416,7 +416,7 @@ const hightLights = computed<Stat[]>(() => ([
             <h3 class="text-xs font-medium tracking-wide text-gray-900 uppercase dark:text-white">
               {{ t('plan-whats-included') }}
             </h3>
-            <ul role="list" class="mt-6 space-y-4">
+            <ul class="mt-6 space-y-4">
               <li v-for="(f, indexx) in planFeatures(p)" :key="indexx" class="flex space-x-3">
                 <svg class="flex-shrink-0 w-5 h-5 text-green-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                   <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd" />
