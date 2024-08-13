579bb4ee559157835cce87cc6a3b41e81a2c9cf5
Martin Donadieu <martindonadieu@gmail.com>
2024-08-13 04:23:25 +0100

    fix: text alt img

--- src/components/Dialog.vue
+++ src/components/Dialog.vue
@@ -77,7 +77,7 @@ onMounted(() => {
         }"
       >
         <p class="text-base leading-relaxed prose text-gray-500 break-words dark:text-gray-400" :class="displayStore.dialogOption?.textStyle" v-html="displayText(displayStore.dialogOption?.message)" />
-        <img v-if="displayStore.dialogOption?.image" alt="dialog image" :src="displayStore.dialogOption?.image" class="ml-auto mr-auto">
+        <img v-if="displayStore.dialogOption?.image" alt="dialog illustration" :src="displayStore.dialogOption?.image" class="ml-auto mr-auto">
         <div v-if="displayStore.dialogOption?.input" class="w-full">
           <FormKit id="dialog-input" type="form" :actions="false" @submit="submit">
             <FormKit
