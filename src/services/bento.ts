8f5198da05f89f5654da530cac8d312e4bc2b89c
Martin Donadieu <martindonadieu@gmail.com>
2024-08-26 22:52:24 +0100

    fix: auto reset bento

--- src/services/bento.ts
+++ src/services/bento.ts
@@ -15,6 +15,12 @@ export function bentoLoader(cb?: () => void) {
       if (cb)
         cb()
     })
+    setTimeout(() => {
+      if (localStorage.getItem('bento:loading') === 'true') {
+        localStorage.setItem('bento:loading', 'false')
+        bentoLoader()
+      }
+    }, 5000)
     return
   }
   else if (window.bento) {
