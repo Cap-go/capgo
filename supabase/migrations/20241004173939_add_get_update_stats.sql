a7ae3a5d23ffc8928fe0ba5a2a7f6e373e2134ce
Martin Donadieu <martindonadieu@gmail.com>
2024-10-14 11:29:14 +0900

    fix: add to logsnag updates-stats

--- supabase/migrations/20241004173939_add_get_update_stats.sql
+++ supabase/migrations/20241004173939_add_get_update_stats.sql
@@ -1,5 +1,6 @@
 CREATE OR REPLACE FUNCTION public.get_update_stats()
 RETURNS TABLE (
+    app_id uuid,
     failed bigint,
     install bigint,
     get bigint,
@@ -9,38 +10,39 @@ RETURNS TABLE (
 LANGUAGE plpgsql
 SECURITY DEFINER
 AS $$
-DECLARE
-    total_events bigint;
-    success_rate numeric;
 BEGIN
-    -- Get the counts for the last minute, 10 minutes ago
+    RETURN QUERY
+    WITH stats AS (
+        SELECT
+            app_id,
+            COALESCE(SUM(CASE WHEN action = 'fail' THEN 1 ELSE 0 END), 0) AS failed,
+            COALESCE(SUM(CASE WHEN action = 'install' THEN 1 ELSE 0 END), 0) AS install,
+            COALESCE(SUM(CASE WHEN action = 'get' THEN 1 ELSE 0 END), 0) AS get
+        FROM
+            version_usage
+        WHERE
+            timestamp >= (date_trunc('minute', now()) - INTERVAL '10 minutes')
+            AND timestamp < (date_trunc('minute', now()) - INTERVAL '9 minutes')
+        GROUP BY
+            app_id
+    )
     SELECT
-        COALESCE(SUM(CASE WHEN action = 'fail' THEN 1 ELSE 0 END), 0) AS failed,
-        COALESCE(SUM(CASE WHEN action = 'install' THEN 1 ELSE 0 END), 0) AS install,
-        COALESCE(SUM(CASE WHEN action = 'get' THEN 1 ELSE 0 END), 0) AS get
-    INTO
-        failed, install, get
+        stats.app_id,
+        stats.failed,
+        stats.install,
+        stats.get,
+        CASE
+            WHEN (stats.failed + stats.install + stats.get) > 0 THEN
+                ROUND(((stats.install + stats.get)::numeric / (stats.failed + stats.install + stats.get)) * 100, 2)
+            ELSE 100
+        END AS success_rate,
+        CASE
+            WHEN (stats.failed + stats.install + stats.get) > 0 THEN
+                (((stats.install + stats.get)::numeric / (stats.failed + stats.install + stats.get)) * 100 >= 70)
+            ELSE true
+        END AS healthy
     FROM
-        version_usage
-    WHERE
-        timestamp >= (date_trunc('minute', now()) - INTERVAL '10 minutes')
-        AND timestamp < (date_trunc('minute', now()) - INTERVAL '9 minutes');
-
-    -- Calculate total events and success rate
-    total_events := failed + install + get;
-    IF total_events > 0 THEN
-        success_rate := ((install + get)::numeric / total_events) * 100;
-    ELSE
-        success_rate := 100;
-    END IF;
-
-    -- Return the result
-    RETURN QUERY SELECT
-        failed,
-        install,
-        get,
-        ROUND(success_rate, 2) AS success_rate,
-        (success_rate >= 70) AS healthy;
+        stats;
 END;
 $$;
 
