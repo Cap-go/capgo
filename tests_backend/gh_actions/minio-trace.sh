ae092a7d1f939fbf35ab924b2f7b144700d0290e
Martin Donadieu <martindonadieu@gmail.com>
2024-10-15 09:26:56 +0900

    chore: remove old configs and rename some

--- tests_backend/gh_actions/minio-trace.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "MinIO trace"
-
-docker run --net=host --entrypoint='/bin/sh' minio/mc -c '/usr/bin/mc alias set minio http://localhost:9000 ROOTUSER CHANGEME123 && /usr/bin/mc admin trace --all --verbose minio'
-
