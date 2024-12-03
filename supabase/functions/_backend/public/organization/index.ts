899d5b6cc39dfa20d593faa47bb169b2899b309a
WcaleNieWolny <michael@capgo.app>
2024-12-03 15:02:42 +0000

    feat: update && create CRUD for orgs

--- supabase/functions/_backend/public/organization/index.ts
+++ supabase/functions/_backend/public/organization/index.ts
@@ -5,6 +5,7 @@ import { get } from './get.ts'
 import { deleteMember } from './members/delete.ts'
 import { get as getMembers } from './members/get.ts'
 import { post as inviteUser } from './members/post.ts'
+import { put } from './put.ts'
 import { post } from './post.ts'
 export const app = new Hono()
 
@@ -16,6 +17,17 @@ app.get('/', middlewareKey(['all', 'write', 'read', 'upload']), async (c: Contex
   }
   catch (e) {
     return c.json({ status: 'Cannot get organization', error: JSON.stringify(e) }, 500)
+  }
+})
+
+app.put('/', middlewareKey(['all', 'write', 'read', 'upload']), async (c: Context) => {
+  try {
+    const body = await getBody<any>(c)
+    const apikey = c.get('apikey')
+    return put(c, body, apikey)
+  }
+  catch (e) {
+    return c.json({ status: 'Cannot create organization', error: JSON.stringify(e) }, 500)
   }
 })
 
