36f864e7e28af7f01d5bb2f7a346a88581d6ec31
Martin Donadieu <martindonadieu@gmail.com>
2024-12-12 02:53:03 +0000

    fix: remove wrong functions

--- supabase/migrations/20241127042617_fix_perms_for_d1_repl.sql
+++ supabase/migrations/20241127042617_fix_perms_for_d1_repl.sql
@@ -3,6 +3,3 @@ GRANT ALL ON FUNCTION "public"."replicate_to_d1"(record jsonb, old_record jsonb,
 
 REVOKE ALL ON FUNCTION "public"."trigger_http_queue_post_to_function_d1"() FROM PUBLIC;
 GRANT ALL ON FUNCTION "public"."trigger_http_queue_post_to_function_d1"() TO "service_role";
-
-REVOKE ALL ON FUNCTION public.get_d1_replication_status FROM PUBLIC;
-GRANT EXECUTE ON FUNCTION public.get_d1_replication_status TO service_role;
