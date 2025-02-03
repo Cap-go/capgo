80b690c013e376c82ad706cad0b86342878de3ef
Martin Donadieu <martindonadieu@gmail.com>
2025-02-03 13:57:16 +0000

    fix: remove useless package

--- scripts/local_cf_backend_env.mjs
+++ scripts/local_cf_backend_env.mjs
@@ -1,86 +1,85 @@
-import { execSync } from 'child_process';
-import { readFileSync, existsSync, writeFileSync } from 'fs';
-import { resolve } from 'path';
-import { exit } from 'process';
-import { parse } from 'dotenv';
+import { existsSync, readFileSync, writeFileSync } from 'node:fs'
+import { resolve } from 'node:path'
+import { argv, exit } from 'node:process'
+import { parse } from 'dotenv'
 
 // Check if the environment file name is provided as a command-line argument
-const envFileName = process.argv[2];
-const envName = process.argv[3];
+const envFileName = argv[2]
+const envName = argv[3]
 if (!envFileName) {
-  console.error('Please provide the environment file name as the second parameter.');
-  exit(1);
+  console.error('Please provide the environment file name as the second parameter.')
+  exit(1)
 }
 if (!envName) {
-  console.error('Please provide the worker name as the third parameter.');
-  exit(1);
+  console.error('Please provide the worker name as the third parameter.')
+  exit(1)
 }
 
 // Resolve and check the existence of the .env file
-const envFilePath = resolve(envFileName);
+const envFilePath = resolve(envFileName)
 if (!existsSync(envFilePath)) {
-  console.error(`Failed to read the environment file at ${envFilePath}.`);
-  exit(1);
+  console.error(`Failed to read the environment file at ${envFilePath}.`)
+  exit(1)
 }
 
-let envContent;
+let envContent
 try {
-  envContent = readFileSync(envFilePath, 'utf8');
-} catch (error) {
-  console.error(`Failed to read the environment file at ${envFilePath}:`, error);
-  exit(1);
+  envContent = readFileSync(envFilePath, 'utf8')
+}
+catch (error) {
+  console.error(`Failed to read the environment file at ${envFilePath}:`, error)
+  exit(1)
 }
 
 // Use dotenv.parse to convert the file content into an object
-const customEnv = parse(envContent);
+const customEnv = parse(envContent)
 
-console.log('Environment file:', envFileName);
-console.log('Worker Name:', envName);
-console.log('Environment variables', customEnv);
-
-
-// Convert the environment variables to a JSON string
-const secrets = JSON.stringify(customEnv, null, 2);
+console.log('Environment file:', envFileName)
+console.log('Worker Name:', envName)
+console.log('Environment variables', customEnv)
 
 // wrangler config file path
-const wranglerPath = resolve('./wrangler.toml');
+const wranglerPath = resolve('./wrangler.toml')
 
 // add variable to wrangler.toml add [vars] section
-const wranglerFile = readFileSync(wranglerPath, 'utf8');
-const wranglerFileLines = wranglerFile.split('\n');
-let varsIndex = wranglerFileLines.findIndex((line) => line.includes('[vars]'));
+const wranglerFile = readFileSync(wranglerPath, 'utf8')
+const wranglerFileLines = wranglerFile.split('\n')
+let varsIndex = wranglerFileLines.findIndex(line => line.includes('[vars]'))
 if (varsIndex === -1) {
-  wranglerFileLines.push('[vars]');
-  varsIndex = wranglerFileLines.length;
-} else {
+  wranglerFileLines.push('[vars]')
+  varsIndex = wranglerFileLines.length
+}
+else {
   // Find the index of the next section after [vars]
   let nextSectionIndex = wranglerFileLines.findIndex((line, index) => {
-    return index > varsIndex && line.startsWith('[');
-  });
+    return index > varsIndex && line.startsWith('[')
+  })
 
   if (nextSectionIndex !== -1) {
     // Remove all lines between [vars] and the next section
-    wranglerFileLines.splice(varsIndex + 1, nextSectionIndex - varsIndex - 1);
-  } else {
+    wranglerFileLines.splice(varsIndex + 1, nextSectionIndex - varsIndex - 1)
+  }
+  else {
     // If no next section, remove everything after [vars]
-    wranglerFileLines.splice(varsIndex + 1);
+    wranglerFileLines.splice(varsIndex + 1)
   }
 }
 
 for (const key in customEnv) {
   // if json string value, escape double quotes
   if (typeof customEnv[key] === 'string') {
-    customEnv[key] = customEnv[key].replace(/"/g, '\\"');
+    customEnv[key] = customEnv[key].replace(/"/g, '\\"')
   }
-  wranglerFileLines.splice(varsIndex + 1, 0, `${key} = "${customEnv[key]}"`);
-  varsIndex++; // Move the index forward after each insertion
+  wranglerFileLines.splice(varsIndex + 1, 0, `${key} = "${customEnv[key]}"`)
+  varsIndex++ // Move the index forward after each insertion
 }
-const newWranglerFile = wranglerFileLines.join('\n');
+const newWranglerFile = wranglerFileLines.join('\n')
 // write new wrangler.toml
 try {
-  writeFileSync(wranglerPath, newWranglerFile);
-  console.log('New wrangler.toml file written successfully');
-} catch (error) {
-  console.error('Failed to upload secrets:', error);
-  exit(1);
+  writeFileSync(wranglerPath, newWranglerFile)
+  console.log('New wrangler.toml file written successfully')
+}
+catch (error) {
+  console.error('Failed to upload secrets:', error)
+  exit(1)
 }
