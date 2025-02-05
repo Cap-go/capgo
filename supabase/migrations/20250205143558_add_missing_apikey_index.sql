ac21786767af1175d536d8fa924577a2a6fd0f5f
Martin Donadieu <martindonadieu@gmail.com>
2025-02-05 19:02:51 +0000

    fix: sql

--- supabase/migrations/20250205143558_add_missing_apikey_index.sql
+++ supabase/migrations/20250205143558_add_missing_apikey_index.sql
@@ -1,1 +1,7 @@
-CREATE INDEX finx_apikeys_user_id ON public.apikeys USING btree (user_id)
+DO $$
+BEGIN
+  IF NOT EXISTS (SELECT 1 FROM pg_indexes WHERE tablename = 'apikeys' AND indexname = 'finx_apikeys_user_id') THEN
+    CREATE INDEX finx_apikeys_user_id ON public.apikeys USING btree (user_id);
+  END IF;
+END
+$$;
