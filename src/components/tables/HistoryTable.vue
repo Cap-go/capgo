d9f437d54bb9b7a5b1cc56469c7c55220d155990
Martin Donadieu <martindonadieu@gmail.com>
2025-04-03 17:29:46 +0200

    fix: remove dup buttons

--- src/components/tables/HistoryTable.vue
+++ src/components/tables/HistoryTable.vue
@@ -272,39 +272,16 @@ watch([() => props.channelId, () => props.appId, sort, page, pageSize, search], 
 </script>
 
 <template>
-  <div>
-    <div class="flex justify-between p-2">
-      <button
-        class="flex items-center p-2 bg-white dark:bg-gray-800 rounded-lg border border-gray-300 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-700"
-        @click="fetchDeployHistory"
-      >
-        <IconReload class="w-5 h-5 mr-1" />
-        {{ t('reload') }}
-      </button>
-      <div class="relative">
-        <input
-          v-model="search"
-          class="w-full pl-10 pr-4 py-2 border rounded-lg bg-white dark:bg-gray-800 dark:border-gray-700"
-          :placeholder="t('search-by-name')"
-          type="text"
-        >
-        <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-          <span v-html="'<svg xmlns=\'http://www.w3.org/2000/svg\' fill=\'none\' viewBox=\'0 0 24 24\' stroke-width=\'1.5\' stroke=\'currentColor\' class=\'w-5 h-5 text-gray-500 dark:text-gray-400\'><path stroke-linecap=\'round\' stroke-linejoin=\'round\' d=\'m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z\' /></svg>'" />
-        </div>
-      </div>
-    </div>
-
-    <Table
-      :is-loading="loading"
-      :search="search"
-      :total="total"
-      :current-page="page"
-      :columns="columns"
-      :element-list="deployHistory"
-      @update:search="search = $event"
-      @update:current-page="page = $event"
-      @update:columns="columns = $event"
-      @reload="fetchDeployHistory"
-    />
-  </div>
+  <Table
+    :is-loading="loading"
+    :search="search"
+    :total="total"
+    :current-page="page"
+    :columns="columns"
+    :element-list="deployHistory"
+    @update:search="search = $event"
+    @update:current-page="page = $event"
+    @update:columns="columns = $event"
+    @reload="fetchDeployHistory"
+  />
 </template>
