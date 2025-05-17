a6a37cb1371d145108867ad183ca54af1dbcc80a
Martin Donadieu <martindonadieu@gmail.com>
2025-05-17 04:29:03 +0100

    fix: perf isssue

--- supabase/migrations/20250516123513_fix_on_app_update.sql
+++ supabase/migrations/20250516123513_fix_on_app_update.sql
@@ -1,2 +1,3 @@
 -- This had to be run manually as the migration cannot modify the net schema
 -- CREATE INDEX idx_http_response_id ON net._http_response(id);
+-- CREATE INDEX ON pgmq.q_on_version_update USING btree (read_ct);
