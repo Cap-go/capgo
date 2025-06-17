50ffd9c60ca409d7a65865e7d8a06cba129faa2c
Jordan Lorho <jordan.lorho@gmail.com>
2025-06-17 15:45:29 +0200

    feat(array-utils): add util functito check array of objects

---
+++ src/utils/arrayUtils.js
@@ -0,0 +1,11 @@
+/**
+ * Checks if exactly one object property in an array matches a given string
+ * @param {Array} array - Array of objects to search through
+ * @param {string} property - Property name to check
+ * @param {string} value - String value to match against
+ * @returns {boolean} - True if exactly one match is found, false otherwise
+ */
+export function hasExactlyOneMatch(array, property, value) {
+  const matches = array.filter(obj => obj[property] === value);
+  return matches.length === 1;
+}
