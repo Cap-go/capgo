433afafc284c7e3aa28b1dd5becdf6826e335d82
Martin Donadieu <martindonadieu@gmail.com>
2025-07-22 05:08:07 +0100

    fix: add 'apikey' to allowed CORS headers for API requests

--- supabase/functions/_backend/utils/hono.ts
+++ supabase/functions/_backend/utils/hono.ts
@@ -21,7 +21,7 @@ import { checkKey, checkKeyById, getEnv } from './utils.ts'
 
 export const useCors = cors({
   origin: '*',
-  allowHeaders: ['Content-Type', 'Authorization', 'capgkey', 'x-api-key', 'x-limited-key-id', 'apisecret'],
+  allowHeaders: ['Content-Type', 'Authorization', 'capgkey', 'x-api-key', 'x-limited-key-id', 'apisecret', 'apikey'],
   allowMethods: ['POST', 'GET', 'OPTIONS'],
 })
 
