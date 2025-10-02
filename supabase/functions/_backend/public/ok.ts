64742284bbb9d6d90dd3a692addbee33e48038b4
Martin Donadieu <martindonadieu@gmail.com>
2025-10-02 17:08:48 +0100

    fix: lint

--- supabase/functions/_backend/public/ok.ts
+++ supabase/functions/_backend/public/ok.ts
@@ -11,8 +11,8 @@ app.post('/', async (c) => {
 
   return apiVersion.handle({
     '2025-10-01': () => c.json(BRES),
-    '2025-10-02': (info) => c.json({ ...BRES, version: info.normalized, detail: 'ok endpoint 2025-10-02 response' }),
-    default: () => c.json(BRES),
+    '2025-10-02': info => c.json({ ...BRES, version: info.normalized, detail: 'ok endpoint 2025-10-02 response' }),
+    'default': () => c.json(BRES),
   })
 })
 
@@ -21,7 +21,7 @@ app.get('/', (c) => {
 
   return apiVersion.handle({
     '2025-10-01': () => c.json(BRES),
-    '2025-10-02': (info) => c.json({ ...BRES, version: info.normalized, detail: 'ok endpoint 2025-10-02 response' }),
-    default: () => c.json(BRES),
+    '2025-10-02': info => c.json({ ...BRES, version: info.normalized, detail: 'ok endpoint 2025-10-02 response' }),
+    'default': () => c.json(BRES),
   })
 })
