c53716413de909bbe4d37bd745e35fbcc83e5e09
Martin Donadieu <martindonadieu@gmail.com>
2025-10-21 19:38:28 +0100

    WIP

---
+++ alicloud/plugin/index.ts
@@ -0,0 +1,32 @@
+import { handle } from 'hono-alibaba-cloud-fc3-adapter'
+import { app as channel_self } from '../../supabase/functions/_backend/plugins/channel_self.ts'
+import { app as stats } from '../../supabase/functions/_backend/plugins/stats.ts'
+import { app as updates } from '../../supabase/functions/_backend/plugins/updates.ts'
+import { app as latency } from '../../supabase/functions/_backend/private/latency.ts'
+import { app as ok } from '../../supabase/functions/_backend/public/ok.ts'
+import { createAllCatch, createHono } from '../../supabase/functions/_backend/utils/hono.ts'
+import { version } from '../../supabase/functions/_backend/utils/version.ts'
+
+export { AttachmentUploadHandler, UploadHandler } from '../../supabase/functions/_backend/tus/uploadHandler.ts'
+
+const functionName = 'plugin'
+const app = createHono(functionName, version, process.env.SENTRY_DSN)
+
+// TODO: deprecated remove when everyone use the new endpoint
+app.route('/plugin/ok', ok)
+app.route('/plugin/channel_self', channel_self)
+app.route('/plugin/updates', updates)
+app.route('/plugin/updates_v2', updates)
+app.route('/plugin/stats', stats)
+
+// Plugin API
+app.route('/channel_self', channel_self)
+app.route('/updates', updates)
+app.route('/updates_v2', updates)
+app.route('/stats', stats)
+app.route('/ok', ok)
+app.route('/latency', latency)
+
+createAllCatch(app, functionName)
+
+export const handler = handle(app)
