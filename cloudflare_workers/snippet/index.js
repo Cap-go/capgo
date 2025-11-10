e53106f291866b37c55a29b425c8e16956897dbf
Martin Donadieu <martindonadieu@gmail.com>
2025-11-10 16:14:36 +0000

    fix: snippet

--- cloudflare_workers/snippet/index.js
+++ cloudflare_workers/snippet/index.js
@@ -2,11 +2,11 @@ export default {
   async fetch(request) {
     /**
      * A map of the URLs to redirect to
-     * @param {Object} countryMap
+     * @param {object} countryMap
      */
     const AS = 'https://plugin.as.capgo.app'
     const EU = 'https://plugin.eu.capgo.app'
-    const NA = 'https://plugin.us.capgo.app'
+    const NA = 'https://plugin.na.capgo.app'
     const SA = 'https://plugin.sa.capgo.app'
     const OC = 'https://plugin.oc.capgo.app'
     const AF = 'https://plugin.af.capgo.app'
@@ -18,26 +18,27 @@ export default {
       AS,
       OC,
       SA,
-      AF
-    };
+      AF,
+    }
 
     // Use the cf object to obtain the country of the request
     // more on the cf object: https://developers.cloudflare.com/workers/runtime-apis/request#incomingrequestcfproperties
-    const continent = request.cf.continent;
+    const continent = request.cf.continent
     const path = new URL(request.url).pathname
-    let baseUrl = continentMap['EU'];
+    let baseUrl = continentMap.EU
     // If country is not null and is defined in the country map above, redirect.
     if (continent != null && continent in continentMap) {
-      baseUrl = continentMap[continent];
+      baseUrl = continentMap[continent]
       // Remove this logging statement from your final output.
       console.log(
         `Based on ${continent}-based request, your user would go to ${baseUrl}.`,
-      );
-      return fetch(`${baseUrl}${path}`, request);
+      )
+      return fetch(`${baseUrl}${path}`, request)
 
       // If request country not in map, return another page.
-    } else {
-      return fetch(`${baseUrl}${path}`, request);
+    }
+    else {
+      return fetch(`${baseUrl}${path}`, request)
     }
   },
-};
+}
