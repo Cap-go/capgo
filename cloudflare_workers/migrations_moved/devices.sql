f619355b482bce40bec3610d35a6f7c166d77700
Martin Donadieu <martindonadieu@gmail.com>
2025-11-11 21:05:09 +0000

    feat: add SQL migration files for devices, stats, and store_apps tables

---
+++ cloudflare_workers/migrations_moved/devices.sql
@@ -0,0 +1,25 @@
+-- DROP TABLE IF EXISTS devices;
+CREATE TABLE IF NOT EXISTS devices (
+    updated_at timestamp with time zone NOT NULL,
+    device_id text NOT NULL,
+    version_name text NOT NULL DEFAULT 'unknown',
+    version bigint NOT NULL,
+    app_id character varying(50) NOT NULL,
+    platform character varying(8) NOT NULL,
+    plugin_version character varying(20) NOT NULL,
+    os_version character varying(20) NOT NULL,
+    default_channel character varying(20) NOT NULL,
+    version_build character varying(20) DEFAULT 'builtin',
+    custom_id character varying(36) DEFAULT '' NOT NULL,
+    is_prod boolean DEFAULT true,
+    is_emulator boolean DEFAULT false,
+    PRIMARY KEY (app_id, device_id)
+);
+
+CREATE INDEX devices_app_id_device_id_updated_at_idx ON devices (app_id, device_id, updated_at);
+
+CREATE INDEX devices_app_id_updated_at_idx ON devices (app_id, updated_at);
+
+CREATE INDEX idx_app_id_created_at_devices ON devices (app_id, updated_at);
+
+CREATE INDEX idx_app_id_version_name_devices ON devices (app_id, version_name);
