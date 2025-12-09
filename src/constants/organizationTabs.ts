3380385fa3cbe8c73ff2896ec457c1e022a1b050
Jordan Lorho <jordan.lorho@gmail.com>
2025-12-09 15:35:31 +0100

    feat: includ credit in the new tab system

--- src/constants/organizationTabs.ts
+++ src/constants/organizationTabs.ts
@@ -3,10 +3,12 @@ import IconChart from '~icons/heroicons/chart-bar'
 import IconPlan from '~icons/heroicons/credit-card'
 import IconInfo from '~icons/heroicons/information-circle'
 import IconUsers from '~icons/heroicons/users'
+import IconCredits from '~icons/heroicons/currency-dollar'
 
 export const organizationTabs: Tab[] = [
   { label: 'general', key: '/settings/organization', icon: IconInfo },
   { label: 'members', key: '/settings/organization/members', icon: IconUsers },
   { label: 'plans', key: '/settings/organization/plans', icon: IconPlan },
   { label: 'usage', key: '/settings/organization/usage', icon: IconChart },
+  { label: 'credits', key: '/settings/organization/credits', icon: IconCredits },
 ]
