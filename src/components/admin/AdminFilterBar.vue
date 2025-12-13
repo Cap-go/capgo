5a118691581b20eed01871538d0d06e6acf88864
Martin Donadieu <martindonadieu@gmail.com>
2025-12-13 09:46:07 +0000

    feat: remove unused computed import from AdminFilterBar component

--- src/components/admin/AdminFilterBar.vue
+++ src/components/admin/AdminFilterBar.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import { computed } from 'vue'
 import { useI18n } from 'vue-i18n'
 import ArrowPathIconSolid from '~icons/heroicons/arrow-path-solid'
 import CalendarDaysIcon from '~icons/heroicons/calendar-days'
