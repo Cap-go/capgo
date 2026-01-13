f0cdbe31a66e2fab17f3f8bd3ec1cef9abf4c628
Jordan Lorho <jordan.lorho@gmail.com>
2026-01-13 12:18:49 +0000

    fix(AppAccess): import Ref

--- src/components/dashboard/AppAccess.vue
+++ src/components/dashboard/AppAccess.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import type { TableColumn } from '~/components/comp_def'
-import { computed, onMounted, ref, watch } from 'vue'
+import { computed, onMounted, ref, watch, Ref } from 'vue'
 import { useI18n } from 'vue-i18n'
 import { toast } from 'vue-sonner'
 import IconInformation from '~icons/heroicons/information-circle'
