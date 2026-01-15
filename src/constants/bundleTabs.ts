e3dbdf9985904110dea35ca83da4af5697504b74
Martin Donadieu <martindonadieu@gmail.com>
2026-01-15 18:47:12 +0000

    Add manifest diff tab UX

--- src/constants/bundleTabs.ts
+++ src/constants/bundleTabs.ts
@@ -1,11 +1,13 @@
 import type { Tab } from '~/components/comp_def'
 import IconHistory from '~icons/heroicons/clock'
+import IconManifest from '~icons/heroicons/clipboard-document-list'
 import IconEye from '~icons/heroicons/eye'
 import IconInfo from '~icons/heroicons/information-circle'
 import IconPuzzle from '~icons/heroicons/puzzle-piece'
 
 export const bundleTabs: Tab[] = [
   { label: 'info', icon: IconInfo, key: '' },
+  { label: 'manifest', icon: IconManifest, key: '/manifest' },
   { label: 'dependencies', icon: IconPuzzle, key: '/dependencies' },
   { label: 'history', icon: IconHistory, key: '/history' },
   { label: 'preview-tab', icon: IconEye, key: '/preview' },
