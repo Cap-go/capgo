8b25b45c58a1ee15c7c4baa38add4e211f910f15
Martin Donadieu <martindonadieu@gmail.com>
2026-01-25 04:12:30 +0000

    fix: remove performance tab and associated component from admin dashboard

--- src/constants/adminTabs.ts
+++ src/constants/adminTabs.ts
@@ -3,7 +3,6 @@ import IconArrowPath from '~icons/heroicons/arrow-path'
 import IconBanknotes from '~icons/heroicons/banknotes'
 import IconChart from '~icons/heroicons/chart-bar'
 import IconCircleStack from '~icons/heroicons/circle-stack'
-import IconClock from '~icons/heroicons/clock'
 import IconCurrencyDollar from '~icons/heroicons/currency-dollar'
 import IconPuzzle from '~icons/heroicons/puzzle-piece'
 import IconUsers from '~icons/heroicons/user-group'
@@ -11,7 +10,6 @@ import IconUsers from '~icons/heroicons/user-group'
 export const adminTabs: Tab[] = [
   { label: 'overview', icon: IconChart, key: '' },
   { label: 'updates', icon: IconArrowPath, key: '/updates' },
-  { label: 'performance', icon: IconClock, key: '/performance' },
   { label: 'replication', icon: IconCircleStack, key: '/replication' },
   { label: 'plugins', icon: IconPuzzle, key: '/plugins' },
   { label: 'users', icon: IconUsers, key: '/users' },
