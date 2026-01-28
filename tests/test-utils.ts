47a7a4d753e161f7cd6da361dc736526ff1135f9
Martin Donadieu <martindonadieu@gmail.com>
2026-01-28 00:46:15 +0000

    test(db): cover delete_user reauth

--- tests/test-utils.ts
+++ tests/test-utils.ts
@@ -50,6 +50,10 @@ export const STRIPE_INFO_CUSTOMER_ID_2 = 'cus_Pa0f3M6UCQ8g5Q' // Customer ID for
 // Dedicated data for email preference tests (isolated to prevent interference)
 export const USER_ID_EMAIL_PREFS = '9f1a2b3c-4d5e-4f60-8a7b-1c2d3e4f5061'
 export const USER_EMAIL_EMAIL_PREFS = 'emailprefs@capgo.app'
+export const USER_ID_DELETE_USER_STALE = 'b7a1d9f4-7b8f-4e3c-8f2b-1a2b3c4d5e6f'
+export const USER_EMAIL_DELETE_USER_STALE = 'delete-user-stale@capgo.app'
+export const USER_ID_DELETE_USER_FRESH = 'c8b2e0f5-8c90-4f4d-9f3c-2b3c4d5e6f70'
+export const USER_EMAIL_DELETE_USER_FRESH = 'delete-user-fresh@capgo.app'
 export const ORG_ID_EMAIL_PREFS = 'aa1b2c3d-4e5f-4a60-9b7c-1d2e3f4a5061'
 export const STRIPE_CUSTOMER_ID_EMAIL_PREFS = 'cus_email_prefs_test_123'
 // Dedicated data for cron/queue tests (isolated per file)
