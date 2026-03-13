c5d998469b72eb55c37f44c72c9b128d38d084fa
Martin Donadieu <martindonadieu@gmail.com>
2026-03-13 14:03:33 +0100

    fix(db): harden app_versions_meta owner_org fallback

--- supabase/tests/45_test_apikey_oracle_permissions.sql
+++ supabase/tests/45_test_apikey_oracle_permissions.sql
@@ -6,7 +6,10 @@ SELECT tests.clear_authentication();
 
 SELECT
     throws_ok(
-        'SELECT get_user_id(''ae6e7458-c46d-4c00-aa3b-153b0b8520ea'')',
+        format(
+            'SELECT get_user_id(%L)',
+            'ae6e7458-c46d-4c00-aa3b-153b0b8520ea'
+        ),
         '42501',
         'permission denied for function get_user_id',
         'get_user_id(testing_key) should not be callable as anon'
@@ -14,7 +17,11 @@ SELECT
 
 SELECT
     throws_ok(
-        'SELECT get_user_id(''ae6e7458-c46d-4c00-aa3b-153b0b8520ea'', ''com.demo.app'')',
+        format(
+            'SELECT get_user_id(%L, %L)',
+            'ae6e7458-c46d-4c00-aa3b-153b0b8520ea',
+            'com.demo.app'
+        ),
         '42501',
         'permission denied for function get_user_id',
         'get_user_id(testing_key, app_id) should not be callable as anon'
@@ -22,7 +29,11 @@ SELECT
 
 SELECT
     throws_ok(
-        'SELECT get_org_perm_for_apikey(''ae6e7458-c46d-4c00-aa3b-153b0b8520ea'', ''com.demo.app'')',
+        format(
+            'SELECT get_org_perm_for_apikey(%L, %L)',
+            'ae6e7458-c46d-4c00-aa3b-153b0b8520ea',
+            'com.demo.app'
+        ),
         '42501',
         'permission denied for function get_org_perm_for_apikey',
         'get_org_perm_for_apikey should not be callable as anon'
@@ -32,7 +43,10 @@ SELECT tests.authenticate_as('test_user');
 
 SELECT
     throws_ok(
-        'SELECT get_user_id(''ae6e7458-c46d-4c00-aa3b-153b0b8520ea'')',
+        format(
+            'SELECT get_user_id(%L)',
+            'ae6e7458-c46d-4c00-aa3b-153b0b8520ea'
+        ),
         '42501',
         'permission denied for function get_user_id',
         'authenticated user should not call get_user_id'
@@ -40,15 +54,23 @@ SELECT
 
 SELECT
     throws_ok(
-        'SELECT get_user_id(''ae6e7458-c46d-4c00-aa3b-153b0b8520ea'', ''com.demo.app'')',
+        format(
+            'SELECT get_user_id(%L, %L)',
+            'ae6e7458-c46d-4c00-aa3b-153b0b8520ea',
+            'com.demo.app'
+        ),
         '42501',
-        'permission denied for function get_user_id',
+        'permission denied for function get_user_id with app_id',
         'authenticated user should not call get_user_id with app_id'
     );
 
 SELECT
     throws_ok(
-        'SELECT get_org_perm_for_apikey(''ae6e7458-c46d-4c00-aa3b-153b0b8520ea'', ''com.demo.app'')',
+        format(
+            'SELECT get_org_perm_for_apikey(%L, %L)',
+            'ae6e7458-c46d-4c00-aa3b-153b0b8520ea',
+            'com.demo.app'
+        ),
         '42501',
         'permission denied for function get_org_perm_for_apikey',
         'authenticated user should not call get_org_perm_for_apikey'
@@ -65,15 +87,16 @@ SELECT
 
 SELECT
     is(
-        get_org_perm_for_apikey('ae6e7458-c46d-4c00-aa3b-153b0b8520ea', 'com.demo.app'),
+        get_org_perm_for_apikey(
+            'ae6e7458-c46d-4c00-aa3b-153b0b8520ea',
+            'com.demo.app'
+        ),
         'perm_owner',
         'service role should still call get_org_perm_for_apikey'
     );
 
 SELECT tests.clear_authentication();
 
-SELECT *
-FROM
-    finish();
+SELECT finish();
 
 ROLLBACK;
