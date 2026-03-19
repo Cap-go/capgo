bcc7ad8bc78f5c16481b5c945a2d8daa05ae7dc6
Martin Donadieu <martindonadieu@gmail.com>
2026-03-19 10:50:19 +0100

    feat(admin): add daily build minute stats

--- supabase/functions/_backend/utils/supabase.types.ts
+++ supabase/functions/_backend/utils/supabase.types.ts
@@ -1133,6 +1133,8 @@ export type Database = {
           builds_last_month: number | null
           builds_last_month_android: number | null
           builds_last_month_ios: number | null
+          build_minutes_day_android: number
+          build_minutes_day_ios: number
           builds_success_android: number | null
           builds_success_ios: number | null
           builds_success_total: number | null
@@ -1194,6 +1196,8 @@ export type Database = {
           builds_last_month?: number | null
           builds_last_month_android?: number | null
           builds_last_month_ios?: number | null
+          build_minutes_day_android?: number
+          build_minutes_day_ios?: number
           builds_success_android?: number | null
           builds_success_ios?: number | null
           builds_success_total?: number | null
@@ -1255,6 +1259,8 @@ export type Database = {
           builds_last_month?: number | null
           builds_last_month_android?: number | null
           builds_last_month_ios?: number | null
+          build_minutes_day_android?: number
+          build_minutes_day_ios?: number
           builds_success_android?: number | null
           builds_success_ios?: number | null
           builds_success_total?: number | null
