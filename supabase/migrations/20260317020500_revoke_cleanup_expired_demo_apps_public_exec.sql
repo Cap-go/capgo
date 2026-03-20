4947f973bb364c6743e09e251a2be10be6363178
Martin Donadieu <martindonadieu@gmail.com>
2026-03-20 02:05:14 +0100

    fix: rename cleanup demo app migration

--- supabase/migrations/20260317020500_revoke_cleanup_expired_demo_apps_public_exec.sql
+++ supabase/migrations/20260317020500_revoke_cleanup_expired_demo_apps_public_exec.sql
@@ -4,6 +4,3 @@ REVOKE ALL ON FUNCTION public.cleanup_expired_demo_apps() FROM ANON;
 
 REVOKE ALL ON FUNCTION public.cleanup_expired_demo_apps()
 FROM AUTHENTICATED;
-
-GRANT EXECUTE ON FUNCTION public.cleanup_expired_demo_apps()
-TO SERVICE_ROLE;
