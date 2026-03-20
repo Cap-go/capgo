1c745903db95bf09c949c3b05f798e93f9dee77c
Martin Donadieu <martindonadieu@gmail.com>
2026-03-20 08:05:03 +0100

    fix: use real service role in test helper

--- supabase/tests/00-supabase_test_helpers.sql
+++ supabase/tests/00-supabase_test_helpers.sql
@@ -259,7 +259,7 @@ CREATE OR REPLACE FUNCTION tests.authenticate_as_service_role()
 RETURNS void
 AS $$
         BEGIN
-            perform set_config('role', 'service_role', true);
+            execute 'set local role service_role';
             perform set_config('request.jwt.claims', null, true);
         END
     $$ LANGUAGE plpgsql;
