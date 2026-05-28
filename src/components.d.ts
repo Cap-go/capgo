c283e8c9e356665318f61da9a1fc5cfb02da5bf1
Martin Donadieu <martindonadieu@gmail.com>
2026-05-28 16:34:31 +0200

    feat(frontend): add builder promotion modal

--- src/components.d.ts
+++ src/components.d.ts
@@ -28,6 +28,7 @@ declare module 'vue' {
     AuthPageShell: typeof import('./components/auth/AuthPageShell.vue')['default']
     Banner: typeof import('./components/Banner.vue')['default']
     BlurBg: typeof import('./components/BlurBg.vue')['default']
+    BuilderPromotionModal: typeof import('./components/BuilderPromotionModal.vue')['default']
     BuildTable: typeof import('./components/tables/BuildTable.vue')['default']
     BundleCompareSelect: typeof import('./components/bundle/BundleCompareSelect.vue')['default']
     BundlePreviewFrame: typeof import('./components/BundlePreviewFrame.vue')['default']
