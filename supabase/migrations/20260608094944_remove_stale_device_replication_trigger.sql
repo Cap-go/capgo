ace275c3ad07f33bbe298d3308d8af2354102718
Martin Donadieu <martindonadieu@gmail.com>
2026-06-08 15:11:32 +0200

    fix: migration

--- supabase/migrations/20260608094944_remove_stale_device_replication_trigger.sql
+++ supabase/migrations/20260608094944_remove_stale_device_replication_trigger.sql
@@ -2,14 +2,3 @@
 -- replicate_data queue. Current migrations never create this trigger, but it
 -- still exists in production from the old replication system.
 DROP TRIGGER IF EXISTS replicate_devices ON public.devices;
-
-DO $$
-BEGIN
-  IF EXISTS (
-    SELECT 1
-    FROM pgmq.list_queues()
-    WHERE queue_name = 'replicate_data'
-  ) THEN
-    PERFORM pgmq.drop_queue('replicate_data');
-  END IF;
-END $$;
