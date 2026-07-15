bef210cd3a9666ac91fc8c588bc64a155fe27882
Martin Donadieu <martindonadieu@gmail.com>
2026-07-15 12:31:37 +0200

    refactor(admin): remove duplicate overview dashboard

--- src/constants/adminTabs.ts
+++ src/constants/adminTabs.ts
@@ -2,7 +2,6 @@ import type { Tab } from '~/components/comp_def'
 import IconArrowPath from '~icons/heroicons/arrow-path'
 import IconBanknotes from '~icons/heroicons/banknotes'
 import IconBuildingOffice from '~icons/heroicons/building-office-2'
-import IconChart from '~icons/heroicons/chart-bar'
 import IconCircleStack from '~icons/heroicons/circle-stack'
 import IconCurrencyDollar from '~icons/heroicons/currency-dollar'
 import IconPuzzle from '~icons/heroicons/puzzle-piece'
@@ -10,7 +9,6 @@ import IconUsers from '~icons/heroicons/user-group'
 import IconBuild from '~icons/heroicons/wrench-screwdriver'
 
 export const adminTabs: Tab[] = [
-  { label: 'overview', icon: IconChart, key: '' },
   { label: 'builder', icon: IconBuild, key: '/builder' },
   { label: 'updates', icon: IconArrowPath, key: '/updates' },
   { label: 'replication', icon: IconCircleStack, key: '/replication' },
